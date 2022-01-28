2e29fe0d57d53d48b08eb391751adad4e51e085e
Hamish Peebles <hamishpeebles@gmail.com>
2022-01-28 08:59:01 +0000

    Export KnownNeuron (#10)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { GovernanceCanister } from "./governance";
+export type { KnownNeuron } from "./governance";
 export { LedgerCanister } from "./ledger";
 export { AccountIdentifier, SubAccount } from "./account_identifier";
 export { ICP } from "./icp";
