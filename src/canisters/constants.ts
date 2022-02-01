eaa7e7d859c953c452b6b7579d8a9d91387a7c90
Max <max@dfinity.org>
2022-02-01 12:03:32 +0100

    +=constants

---
+++ src/canisters/constants.ts
@@ -0,0 +1,10 @@
+export const SUB_ACCOUNT_BYTE_LENGTH = 32;
+export const CREATE_CANISTER_MEMO = BigInt(0x41455243); // CREA,
+export const TOP_UP_CANISTER_MEMO = BigInt(0x50555054); // TPUP
+
+export const TRANSACTION_FEE = BigInt(10_000);
+
+// Note:  Canister IDs are not constant, so are not provided here.
+
+// @ts-ignore
+export const HOST = "HOST" in config ? config["HOST"] : undefined;
--- src/canisters/converter.ts
+++ src/canisters/converter.ts
@@ -1,3 +1,6 @@
 import { Principal } from "@dfinity/principal";
 import { sha224 } from "js-sha256";
+import { Buffer } from "buffer";
+// @ts-ignore (no type definitions for crc are available)
+import crc from "crc";
 
