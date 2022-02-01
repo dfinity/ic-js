6f0f14a442d4fe37af31f6b3b82b361276579efe
Max <max@dfinity.org>
2022-02-01 12:04:44 +0100

    All common converter imports

--- src/canisters/converter.ts
+++ src/canisters/converter.ts
@@ -3,4 +3,6 @@ import { sha224 } from "js-sha256";
 import { Buffer } from "buffer";
 // @ts-ignore (no type definitions for crc are available)
 import crc from "crc";
+import { SUB_ACCOUNT_BYTE_LENGTH } from "./constants";
+import { AccountIdentifier, SubAccount } from "./common/types";
 
