a533b42ebd328dddbf42840db5c659bf45dea572
Max <max@dfinity.org>
2022-02-02 10:29:50 +0100

    Add checks

--- candid/governance.idl.js
+++ candid/governance.idl.js
@@ -1,20 +1,19 @@
-export const idlFactory = ({ IDL }) => {
-  const NeuronId = IDL.Record({ id: IDL.Nat64 });
+/* Do not edit.  Compiled with didc from candid/governance.did */
+export default ({ IDL }) => {
+  const NeuronId = IDL.Record({ 'id' : IDL.Nat64 });
   const KnownNeuronData = IDL.Record({
-    name: IDL.Text,
-    description: IDL.Opt(IDL.Text),
+    'name' : IDL.Text,
+    'description' : IDL.Opt(IDL.Text),
   });
   const KnownNeuron = IDL.Record({
-    id: IDL.Opt(NeuronId),
-    known_neuron_data: IDL.Opt(KnownNeuronData),
+    'id' : IDL.Opt(NeuronId),
+    'known_neuron_data' : IDL.Opt(KnownNeuronData),
   });
   const ListKnownNeuronsResponse = IDL.Record({
-    known_neurons: IDL.Vec(KnownNeuron),
+    'known_neurons' : IDL.Vec(KnownNeuron),
   });
   return IDL.Service({
-    list_known_neurons: IDL.Func([], [ListKnownNeuronsResponse], ["query"]),
+    'list_known_neurons' : IDL.Func([], [ListKnownNeuronsResponse], ['query']),
   });
 };
-export const init = ({ IDL }) => {
-  return [];
-};
+export const init = ({ IDL }) => { return []; };
