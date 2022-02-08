22a2c873954c425fc9d1d8fe1067f10f60655e6a
peterpeterparker <david.dalbusco@outlook.com>
2022-02-08 08:25:55 +0100

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export { GovernanceCanister } from "./governance";
-export type { KnownNeuron } from "./types/governance_converters";
 export { LedgerCanister } from "./ledger";
 export { AccountIdentifier, SubAccount } from "./account_identifier";
 export { ICP } from "./icp";
+export * from "./types/governance";
+export * from "./types/governance_converters";
+export * from "./types/icp";
+export * from "./types/ledger";
