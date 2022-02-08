4f45d2ceddbbaff300b348865e5f34dfce31c646
peterpeterparker <david.dalbusco@outlook.com>
2022-02-08 08:13:14 +0100

    fix: export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { GovernanceCanister } from "./governance";
-export type { KnownNeuron } from "./governance";
+export type { KnownNeuron } from "./types/governance_converters";
 export { LedgerCanister } from "./ledger";
 export { AccountIdentifier, SubAccount } from "./account_identifier";
 export { ICP } from "./icp";
