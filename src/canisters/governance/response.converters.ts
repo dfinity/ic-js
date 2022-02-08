b04090dc6be2ed31ab146c1aac0b8736596e3573
peterpeterparker <david.dalbusco@outlook.com>
2022-02-08 07:29:44 +0100

    refactor: payloads

--- src/canisters/governance/response.converters.ts
+++ src/canisters/governance/response.converters.ts
@@ -68,7 +68,7 @@ import { Option } from "../../types/option";
 import {
   convertNnsFunctionPayload,
   getNnsFunctionName,
-} from "./nnsFunctions/nnsFunctions";
+} from "../payloads/payloads";
 import { UnsupportedValueError } from "../../types/error";
 
 export class ResponseConverters {
