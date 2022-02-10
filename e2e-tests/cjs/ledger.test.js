6949edd170e60f7f2bfeb441c5d2bd45ca670729
Max <max@dfinity.org>
2022-02-10 15:42:00 +0100

    Put the account ID in a config file

--- e2e-tests/cjs/ledger.test.js
+++ e2e-tests/cjs/ledger.test.js
@@ -2,6 +2,9 @@
  * @jest-environment jsdom
  */
 
+const network = process.env.NETWORK || "mainnet";
+const config = require("../config").networks[network];
+
 const nns = require("../../dist"); // in real use cases:  require("@dfinity/nns");
 const { AccountIdentifier, LedgerCanister } = nns;
 
@@ -9,9 +12,8 @@ test("Should be able to get a balance", () => {
   jest.setTimeout(30000);
   const ledger = LedgerCanister.create();
 
-  const accountIdentifier = AccountIdentifier.fromHex(
-    "268e03eca92149dc91993f843e29aa24b02a55f714b27a3429f40865541b74c5"
-  );
+  let anAccount = config.accounts[0];
+  const accountIdentifier = AccountIdentifier.fromHex(anAccount);
 
   return ledger
     .accountBalance({ accountIdentifier })
