dbfd2424955035bb34efaf636dafd2d0a2d9eeec
Max <max@dfinity.org>
2022-02-11 12:00:45 +0100

    ++

--- src/governance.ts
+++ src/governance.ts
@@ -18,8 +18,8 @@ import { defaultAgent } from "./utils/agent.utils";
 
 // TODO: Should this be renamed to something like GovernanceClient or GovernanceApi or GovernanceService?  The governance canister is something running wasm on the IC, not this code which runs in a browser.
 export class GovernanceCanister {
-  canisterId: Principal;
-  myPrincipal: Principal | undefined;
+  readonly canisterId: Principal;
+  readonly myPrincipal: Principal | undefined;
 
   private constructor(
     private readonly service: GovernanceService,
