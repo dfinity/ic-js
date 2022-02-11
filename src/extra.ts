5d4d40f096c253549c01436edfb04399ed39eeee
Max <max@dfinity.org>
2022-02-11 05:03:44 +0100

    format

--- src/extra.ts
+++ src/extra.ts
@@ -2,19 +2,21 @@
 // - calls that require multiple canisters.
 // - calls that require additional systems such as hardware wallets.
 
-import { CreateNeuronRequest } from "./types/extra";
 import { Principal } from "@dfinity/principal";
 import { LedgerCanister } from "./ledger";
 import { NnsDappCanister } from "./nns_dapp";
+import { CreateNeuronRequest } from "./types/extra";
 
 /**
  * Uses governance and ledger canisters to create a neuron.
  */
 export async function createNeuronWithNnsDapp({
-    request
-  }: {  principal: Principal,
-  ledgerCanister: LedgerCanister,
-  nnsDappCanister: NnsDappCanister,
-request: CreateNeuronRequest}): Promise<string> {
-     return new Promise(() => "foo");
+  request,
+}: {
+  principal: Principal;
+  ledgerCanister: LedgerCanister;
+  nnsDappCanister: NnsDappCanister;
+  request: CreateNeuronRequest;
+}): Promise<string> {
+  return new Promise(() => "foo");
 }
--- src/types/extra.ts
+++ src/types/extra.ts
@@ -1,12 +1,8 @@
 // Types used for specific inter-canister operations.  These types are "uncommon" as they are not ever likely to be useful for other canisters.
 
-import {
-  E8s,
-} from "./common";
-
+import { E8s } from "./common";
 
 export type CreateNeuronRequest = {
   stake: E8s;
   fromSubAccountId?: number;
 };
-
