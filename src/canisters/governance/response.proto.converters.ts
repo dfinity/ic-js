b49a7efbef0eaf96ccf5bac4e34af19ac91987a6
Llorenç <llorenc.muntaner@dfinity.org>
2022-05-13 14:19:43 +0200

    L2-486: throw GovernanceError

--- src/canisters/governance/response.proto.converters.ts
+++ src/canisters/governance/response.proto.converters.ts
@@ -1,9 +1,13 @@
 import { ManageNeuronResponse as PbManageNeuronResponse } from "../../../proto/governance_pb";
+import { GovernanceError } from "../../errors/governance.errors";
 
 export const checkPbManageNeuronResponse = (rawResponse: Uint8Array): void => {
   const response = PbManageNeuronResponse.deserializeBinary(rawResponse);
   const err = response.getError();
   if (err) {
-    throw err;
+    throw new GovernanceError({
+      error_message: err.getErrorMessage(),
+      error_type: err.getErrorType(),
+    });
   }
 };
