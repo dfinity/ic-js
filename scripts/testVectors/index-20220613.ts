a5ee2e453e72e805f1aefd8fbe0e3afd6df1cbbd
Llorenç <llorenc.muntaner@dfinity.org>
2022-06-17 13:35:17 +0200

    Chore: Test Vectos Send ICPs

--- scripts/testVectors/index-20220613.ts
+++ scripts/testVectors/index-20220613.ts
@@ -1,14 +1,5 @@
-import {
-  AnonymousIdentity,
-  CallRequest,
-  Cbor,
-  Expiry,
-  ReadRequest,
-  SubmitRequestType,
-} from "@dfinity/agent";
 import { IDL } from "@dfinity/candid";
 import { Principal } from "@dfinity/principal";
-import { writeFileSync } from "fs";
 import { NeuronId as PbNeuronId } from "../../proto/base_types_pb";
 import {
   ListNeurons as PbListNeurons,
@@ -39,38 +30,9 @@ import {
   fromStartDissolvingRequest,
   fromStopDissolvingRequest,
 } from "../../src/canisters/governance/request.proto.converters";
-import { MAINNET_GOVERNANCE_CANISTER_ID } from "../../src/constants/canister_ids";
-import { ListNeuronsFn, ManageNeuronFn } from "./candid";
+import { ListNeuronsFn, ManageNeuronFn } from "./governanceCandid";
+import { createBlob, writeToJson } from "./utils";
 
-/**
- * Changes needed to match the `arg` inside the blob_proto with the Hardware Wallet CLI blob.
- *
- * In this file
- * - Change `sender` in the `createCallRequest` to the one commented.
- *
- * In the hardware-wallet-cli
- * - `disableNonce: true` in `getAgent` when creating the `new HttpAgent(...)`.
- * - `console.log(Buffer.from(blob).toString("hex"));` inside the `sign` method in LedgerIdentity
- *
- * Go to cbor.me and decode both blobs.
- * The `arg` property should match.
- */
-
-/**
- * Extra change to check for `candid`.
- *
- * In the hardware-wallet-cli
- * - `hardwareWallet: false` in the method.
- */
-
-function _prepareCborForLedger(
-  request: ReadRequest | CallRequest
-): ArrayBuffer {
-  return Cbor.encode({ content: request });
-}
-
-// Default delta for ingress expiry is 5 minutes.
-const DEFAULT_INGRESS_EXPIRY_DELTA_IN_MSECS = 5 * 60 * 1000;
 const mockNeuronId = BigInt(15374508381553347371);
 const mockNeuronId2 = BigInt(8836564053576662908);
 const mockAccountId =
@@ -78,39 +40,6 @@ const mockAccountId =
 const mockPrincipal = Principal.fromText(
   "xlmdg-vkosz-ceopx-7wtgu-g3xmd-koiyc-awqaq-7modz-zf6r6-364rh-oqe"
 );
-
-const createCallRequest = ({
-  arg,
-  methodName,
-}: {
-  arg: ArrayBuffer;
-  methodName: string;
-}): CallRequest => ({
-  request_type: SubmitRequestType.Call,
-  canister_id: MAINNET_GOVERNANCE_CANISTER_ID,
-  method_name: methodName,
-  arg,
-  sender: new AnonymousIdentity().getPrincipal(),
-  // sender: Principal.fromText(
-  //   "q2fbu-chrgb-o7dmd-u5cfn-xgo4f-5lpcl-ldeca-wl4sn-5j7gb-ltm63-hqe"
-  // ),
-  ingress_expiry: new Expiry(DEFAULT_INGRESS_EXPIRY_DELTA_IN_MSECS),
-});
-
-const createBlob = ({
-  arg,
-  methodName,
-}: {
-  arg: ArrayBuffer;
-  methodName: string;
-}): string => {
-  const callRequestCandid = createCallRequest({
-    arg,
-    methodName,
-  });
-  const candidBlob = _prepareCborForLedger(callRequestCandid);
-  return Buffer.from(candidBlob).toString("hex");
-};
 
 const createListNeuronsVector = () => {
   // CANDID
@@ -362,18 +291,10 @@ const main = () => {
       createEditFolloweesVector(),
     ];
 
-    writeFileSync(
-      "test-vectors-20220613.json",
-      JSON.stringify(vectors, (key, value) => {
-        if (typeof value === "bigint") {
-          return value.toString();
-        }
-        if (value instanceof Principal) {
-          return value.toText();
-        }
-        return value;
-      })
-    );
+    writeToJson({
+      data: vectors,
+      fileName: "test-vectors-20220613.json",
+    });
     console.log("File created successfully");
   } catch (error) {
     console.log("There was an error");
---
+++ scripts/testVectors/index-20220617.ts
@@ -0,0 +1,109 @@
+import { IDL } from "@dfinity/candid";
+import { Memo, Payment, SendRequest } from "../../proto/ledger_pb";
+import { AccountIdentifier } from "../../src/account_identifier";
+import {
+  toICPTs,
+  toTransferRawRequest,
+} from "../../src/canisters/ledger/ledger.request.converts";
+import { ICP } from "../../src/icp";
+import { TransferFn } from "./ledgerCandid";
+import { createBlob, writeToJson } from "./utils";
+
+const mockAccountIdentifier = AccountIdentifier.fromHex(
+  "646f4d2d6fcb6fab5ba1547647526b666553467ecb5cb28c8d9ddf451c8f4c21"
+);
+const mockTo = AccountIdentifier.fromHex(
+  "b1cebc8480a0afc91198a87ddf52c6ca7eb7ccddb0cb398064f71d2bbaf2f72b"
+);
+const arrayBufferToNumber = (buffer: ArrayBuffer): number => {
+  const view = new DataView(buffer);
+  return view.getUint32(view.byteLength - 4);
+};
+export const toSubAccountId = (subAccount: number[]): number => {
+  const bytes = new Uint8Array(subAccount).buffer;
+  return arrayBufferToNumber(bytes);
+};
+const mockSubaccountId = toSubAccountId([
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1,
+]);
+
+// A helper to decode candid. Useful to check that the payload is what is supposed to be.
+// const decodeCandid = () => {
+//   const payload =
+//     "4449444C066D7B6C01E0A9B302786E006C01D6F68E8001786E036C06FBCA0100C6FCB60201BA89E5C20478A2DE94EB060282F3F3910C04D8A38CA80D01010520B1CEBC8480A0AFC91198A87DDF52C6CA7EB7CCDDB0CB398064F71D2BBAF2F72B10270000000000000000000000000000000000CA9A3B00000000";
+//   const decoded = IDL.decode(TransferFn.argTypes, Buffer.from(payload, "hex"));
+//   console.log(decoded);
+// };
+
+// decodeCandid();
+
+const createTransferVector = ({
+  to,
+  amount,
+  fromSubAccountId,
+}: {
+  to: AccountIdentifier;
+  amount: number;
+  fromSubAccountId?: number;
+}) => {
+  const request = {
+    to,
+    amount: ICP.fromE8s(BigInt(amount)),
+    fee: BigInt(10_000),
+    memo: BigInt(0),
+    fromSubAccountId,
+  };
+  // CANDID
+  const rawRequestBody = toTransferRawRequest(request);
+  // PROTO
+  const requestPb = new SendRequest();
+  requestPb.setTo(request.to.toProto());
+
+  const payment = new Payment();
+  payment.setReceiverGets(request.amount.toProto());
+  requestPb.setPayment(payment);
+
+  requestPb.setMaxFee(toICPTs(request.fee));
+
+  // Always explicitly set the memo for compatibility with ledger wallet - hardware wallet
+  const requestMemo = new Memo();
+  requestMemo.setMemo((request.memo ?? BigInt(0)).toString());
+  requestPb.setMemo(requestMemo);
+  return {
+    blob_candid: createBlob({
+      arg: IDL.encode(TransferFn.argTypes, [rawRequestBody]),
+      methodName: "transfer",
+    }),
+    blob_proto: createBlob({
+      arg: requestPb.serializeBinary(),
+      methodName: "send_pb",
+    }),
+    name: "Send ICPs",
+    candid_request: rawRequestBody,
+  };
+};
+
+const main = () => {
+  try {
+    const vectors = [
+      { to: mockAccountIdentifier, amount: 100_000_000 },
+      { to: mockAccountIdentifier, amount: 10_000_000_000 },
+      { to: mockTo, amount: 400_000_000 },
+      { to: mockTo, amount: 123_000_000 },
+      { to: mockTo, amount: 123_000_000, fromSubAccountId: mockSubaccountId },
+      { to: mockTo, amount: 123_000_000, fromSubAccountId: mockSubaccountId },
+    ].map(createTransferVector);
+
+    writeToJson({
+      data: vectors,
+      fileName: "test-vectors-20220617.json",
+    });
+    console.log("File created successfully");
+  } catch (error) {
+    console.log("There was an error");
+    console.log(error);
+  }
+};
+
+main();
