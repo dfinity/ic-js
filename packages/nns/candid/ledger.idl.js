031aac9e7e37862195fcd4a1440b10b8dfaff371
David Dal Busco <david.dalbusco@dfinity.org>
2022-07-04 11:04:09 +0200

    refactor: mono repo (#126)
    # Motivation
    
    As we aim and have to develop more libraries such as `sns-js` (current [repo](https://github.com/dfinity/sns-js)) it is more convenient to handle and maintain all our libraries with a mono-repo.
    
    This PR transform the existing repo into a mono-repo using npm workspaces (as already in [agent-js](https://github.com/dfinity/agent-js/)).
    
    # Changes
    
    - introduces npm workspaces and workspace `packages`
    - refactor `nns-js` library to `packages/nns-js`
    - adapt scripts accordingly
    - prepare renaming of repo to `ic-js`

--- packages/nns/candid/ledger.idl.js
+++ packages/nns/candid/ledger.idl.js
@@ -1,13 +1,16 @@
-/* Do not edit.  Compiled with ./scripts/compile-idl-js from candid/ledger.did */
+/* Do not edit.  Compiled with ./scripts/compile-idl-js from packages/nns/candid/ledger.did */
 export const idlFactory = ({ IDL }) => {
+  const AccountIdentifier = IDL.Vec(IDL.Nat8);
+  const AccountBalanceArgs = IDL.Record({ 'account' : AccountIdentifier });
+  const Tokens = IDL.Record({ 'e8s' : IDL.Nat64 });
+  const Archive = IDL.Record({ 'canister_id' : IDL.Principal });
+  const Archives = IDL.Record({ 'archives' : IDL.Vec(Archive) });
   const BlockIndex = IDL.Nat64;
   const GetBlocksArgs = IDL.Record({
     'start' : BlockIndex,
     'length' : IDL.Nat64,
   });
   const Memo = IDL.Nat64;
-  const AccountIdentifier = IDL.Vec(IDL.Nat8);
-  const Tokens = IDL.Record({ 'e8s' : IDL.Nat64 });
   const Operation = IDL.Variant({
     'Burn' : IDL.Record({ 'from' : AccountIdentifier, 'amount' : Tokens }),
     'Mint' : IDL.Record({ 'to' : AccountIdentifier, 'amount' : Tokens }),
@@ -44,7 +47,11 @@ export const idlFactory = ({ IDL }) => {
     'Ok' : BlockRange,
     'Err' : QueryArchiveError,
   });
-  const QueryArchiveFn = IDL.Func([GetBlocksArgs], [QueryArchiveResult], []);
+  const QueryArchiveFn = IDL.Func(
+      [GetBlocksArgs],
+      [QueryArchiveResult],
+      ['query'],
+    );
   const QueryBlocksResponse = IDL.Record({
     'certificate' : IDL.Opt(IDL.Vec(IDL.Nat8)),
     'blocks' : IDL.Vec(Block),
@@ -58,9 +65,6 @@ export const idlFactory = ({ IDL }) => {
       })
     ),
   });
-  const AccountBalanceArgs = IDL.Record({ 'account' : AccountIdentifier });
-  const Archive = IDL.Record({ 'canister_id' : IDL.Principal });
-  const Archives = IDL.Record({ 'archives' : IDL.Vec(Archive) });
   const SubAccount = IDL.Vec(IDL.Nat8);
   const TransferArgs = IDL.Record({
     'to' : AccountIdentifier,
@@ -84,14 +88,22 @@ export const idlFactory = ({ IDL }) => {
   const TransferFeeArg = IDL.Record({});
   const TransferFee = IDL.Record({ 'transfer_fee' : Tokens });
   return IDL.Service({
-    '_blocks' : IDL.Func([GetBlocksArgs], [QueryBlocksResponse], []),
-    'account_balance' : IDL.Func([AccountBalanceArgs], [Tokens], []),
-    'archives' : IDL.Func([], [Archives], []),
-    'decimals' : IDL.Func([], [IDL.Record({ 'decimals' : IDL.Nat32 })], []),
-    'name' : IDL.Func([], [IDL.Record({ 'name' : IDL.Text })], []),
-    'symbol' : IDL.Func([], [IDL.Record({ 'symbol' : IDL.Text })], []),
+    'account_balance' : IDL.Func([AccountBalanceArgs], [Tokens], ['query']),
+    'archives' : IDL.Func([], [Archives], ['query']),
+    'decimals' : IDL.Func(
+        [],
+        [IDL.Record({ 'decimals' : IDL.Nat32 })],
+        ['query'],
+      ),
+    'name' : IDL.Func([], [IDL.Record({ 'name' : IDL.Text })], ['query']),
+    'query_blocks' : IDL.Func(
+        [GetBlocksArgs],
+        [QueryBlocksResponse],
+        ['query'],
+      ),
+    'symbol' : IDL.Func([], [IDL.Record({ 'symbol' : IDL.Text })], ['query']),
     'transfer' : IDL.Func([TransferArgs], [TransferResult], []),
-    'transfer_fee' : IDL.Func([TransferFeeArg], [TransferFee], []),
+    'transfer_fee' : IDL.Func([TransferFeeArg], [TransferFee], ['query']),
   });
 };
 export const init = ({ IDL }) => { return []; };
--- scripts/update_proto.sh
+++ scripts/update_proto.sh
@@ -4,12 +4,12 @@ protoc \
   --plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts" \
   --ts_out="./" \
   --js_out="import_style=commonjs,binary:./" \
-  --proto_path="./" \
-  ./proto/base_types.proto
+  --proto_path="./packages/nns/" \
+  ./packages/nns/proto/base_types.proto
 
 protoc \
   --plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts" \
   --ts_out="./proto" \
   --js_out="import_style=commonjs,binary:./proto" \
-  --proto_path="./proto" \
-  ./proto/ledger.proto
+  --proto_path="./packages/nns/proto" \
+  ./packages/nns/proto/ledger.proto
