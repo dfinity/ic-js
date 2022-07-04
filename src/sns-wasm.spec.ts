0259dc60c5780669bd0f1526b22896a93d0df737
David Dal Busco <david.dalbusco@dfinity.org>
2022-07-04 06:55:25 +0200

    refactor: extract mock

--- src/sns-wasm.spec.ts
+++ src/sns-wasm.spec.ts
@@ -1,27 +1,19 @@
-import { Principal } from "@dfinity/principal";
 import { mock } from "jest-mock-extended";
-import { DeployedSns, SnsWasmService } from "../candid/sns-wasm.idl";
+import { SnsWasmService } from "../candid/sns-wasm.idl";
+import { snsMock } from "./mocks/sns-wasm.mock";
 import { SnsWasmCanister } from "./sns-wasm";
 
 describe("Sns-wasm", () => {
   it("should return the list of sns", async () => {
-    const sns: DeployedSns[] = [
-      {
-        root_canister_id: [Principal.fromText("rrkah-fqaaa-aaaaa-aaaaq-cai")],
-      },
-      {
-        root_canister_id: [Principal.fromText("ryjl3-tyaaa-aaaaa-aaaba-cai")],
-      },
-    ];
     const service = mock<SnsWasmService>();
     service.list_deployed_snses.mockResolvedValue({
-      instances: sns,
+      instances: snsMock,
     });
 
     const canister = SnsWasmCanister.create({
       certifiedServiceOverride: service,
     });
     const res = await canister.listSns({});
-    expect(res).toEqual(sns);
+    expect(res).toEqual(snsMock);
   });
 });
