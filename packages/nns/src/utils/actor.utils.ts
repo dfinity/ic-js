a0e82b4865fec7e6e73a88991f96bbdf987258ba
David Dal Busco <david.dalbusco@outlook.com>
2022-07-05 13:33:40 +0200

    refactor: services initialization and canister options (#133)
    # Motivation
    
    Refactor services and actor options and initialization to avoid duplicate code. 
    
    # Changes
    
    - introduces generic interface `CanisterOptions` and utilities to initialize the services for the options and idl factory
    - update services accordingly

---
+++ packages/nns/src/utils/actor.utils.ts
@@ -0,0 +1,47 @@
+import type { ActorSubclass, Agent } from "@dfinity/agent";
+import { Actor } from "@dfinity/agent";
+import type { IDL } from "@dfinity/candid";
+import type { CanisterOptions } from "../types/canister.options";
+import { defaultAgent } from "./agent.utils";
+
+type RequiredCanisterOptions<T> = Required<
+  Pick<CanisterOptions<T>, "canisterId">
+> &
+  Omit<CanisterOptions<T>, "canisterId">;
+
+export const createServices = <T>({
+  options: {
+    canisterId,
+    serviceOverride,
+    certifiedServiceOverride,
+    agent: agentOption,
+  },
+  idlFactory,
+  certifiedIdlFactory,
+}: {
+  options: RequiredCanisterOptions<T>;
+  idlFactory: IDL.InterfaceFactory;
+  certifiedIdlFactory: IDL.InterfaceFactory;
+}): {
+  service: ActorSubclass<T>;
+  certifiedService: ActorSubclass<T>;
+  agent: Agent;
+} => {
+  const agent: Agent = agentOption ?? defaultAgent();
+
+  const service: ActorSubclass<T> =
+    serviceOverride ??
+    Actor.createActor<T>(idlFactory, {
+      agent,
+      canisterId,
+    });
+
+  const certifiedService: ActorSubclass<T> =
+    certifiedServiceOverride ??
+    Actor.createActor<T>(certifiedIdlFactory, {
+      agent,
+      canisterId,
+    });
+
+  return { service, certifiedService, agent };
+};
