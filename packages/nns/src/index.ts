b51102cbc7bbfdf1ef4834eb84606afa14ff3761
Formatting Committer <nobody@example.com>
2022-07-06 15:33:29 +0000

    Updating formatting

--- packages/nns/src/index.ts
+++ packages/nns/src/index.ts
@@ -6,13 +6,13 @@ export { GenesisTokenCanister } from "./genesis_token";
 export { GovernanceCanister } from "./governance";
 export { ICP } from "./icp";
 export { LedgerCanister } from "./ledger";
+export type { SnsWasmCanister } from "./sns_wasm";
 export * from "./types/common";
 export * from "./types/governance.options";
 export * from "./types/governance_converters";
 export * from "./types/icp";
 export * from "./types/ledger.options";
+export type { SnsWasmCanisterOptions } from "./types/sns_wasm.options";
 export * from "./utils/accounts.utils";
 export * from "./utils/account_identifier.utils";
 export * from "./utils/neurons.utils";
-export type {SnsWasmCanister} from "./sns_wasm"
-export type {SnsWasmCanisterOptions} from './types/sns_wasm.options'
--- packages/nns/src/types/sns_wasm.options.ts
+++ packages/nns/src/types/sns_wasm.options.ts
@@ -1,4 +1,4 @@
-import type {CanisterOptions} from './canister.options';
-import type {_SERVICE as SnsWasmService} from '../../candid/sns_wasm';
+import type { _SERVICE as SnsWasmService } from "../../candid/sns_wasm";
+import type { CanisterOptions } from "./canister.options";
 
 export type SnsWasmCanisterOptions = CanisterOptions<SnsWasmService>;
