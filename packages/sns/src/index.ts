8804c0474ac6106002142437077620e45a303fd3
peterpeterparker <david.dalbusco@outlook.com>
2022-07-12 12:28:53 +0200

    build: export all chunks into main too

--- packages/sns/src/index.ts
+++ packages/sns/src/index.ts
@@ -1,7 +1,9 @@
 export type { Neuron, NeuronId } from "../candid/sns_governance";
 export type { CanisterStatusResultV2 } from "../candid/sns_root";
-export type { InitSns, InitSnsCanistersOptions } from "./sns";
-export type { SnsWrapper } from "./sns.wrapper";
+export * from "./governance.canister";
+export * from "./root.canister";
+export * from "./sns";
+export * from "./sns.wrapper";
 export type { CanisterOptions } from "./types/canister.options";
 export type { ListNeuronsParams } from "./types/governance.params";
 export type { QueryParams } from "./types/query.params";
