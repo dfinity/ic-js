0e6caf20a5323f980f67b7f567f4502a1759f044
peterpeterparker <david.dalbusco@outlook.com>
2022-07-12 12:43:02 +0200

    build: named export for canisters

--- packages/sns/src/index.ts
+++ packages/sns/src/index.ts
@@ -1,7 +1,8 @@
 export type { Neuron, NeuronId } from "../candid/sns_governance";
 export type { CanisterStatusResultV2 } from "../candid/sns_root";
-export * from "./governance.canister";
-export * from "./root.canister";
+export { GovernanceCanister } from "./governance.canister";
+export { LedgerCanister } from "./ledger.canister";
+export { RootCanister } from "./root.canister";
 export * from "./sns";
 export * from "./sns.wrapper";
 export type { CanisterOptions } from "./types/canister.options";
