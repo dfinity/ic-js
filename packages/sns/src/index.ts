0fa8a52f4a26d52c905d50a9d2b62c8a633e0ae4
peterpeterparker <david.dalbusco@outlook.com>
2022-07-13 12:29:10 +0200

    feat: lib export

--- packages/sns/src/index.ts
+++ packages/sns/src/index.ts
@@ -1,10 +1,12 @@
 export type { Neuron, NeuronId } from "../candid/sns_governance";
 export type { CanisterStatusResultV2 } from "../candid/sns_root";
+export type { DerivedState, Swap } from "../candid/sns_swap";
 export { GovernanceCanister } from "./governance.canister";
 export { LedgerCanister } from "./ledger.canister";
 export { RootCanister } from "./root.canister";
 export * from "./sns";
 export * from "./sns.wrapper";
+export { SwapCanister } from "./swap.canister";
 export type { CanisterOptions } from "./types/canister.options";
 export type { ListNeuronsParams } from "./types/governance.params";
 export type { QueryParams } from "./types/query.params";
