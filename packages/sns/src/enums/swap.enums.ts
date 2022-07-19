11d6b9086d5a4c01c6aaa521263c5d5e9d965d29
David Dal Busco <david.dalbusco@outlook.com>
2022-07-19 16:05:37 +0200

    feat: expose more sns types for the swap (#153)
    # Motivation
    
    More candid type exposed for sns swap and define missing candid enums
    
    # Changes
    
    - expose more types
    - define enum for swap state

---
+++ packages/sns/src/enums/swap.enums.ts
@@ -0,0 +1,8 @@
+// Source: https://github.com/dfinity/ic/blob/master/rs/sns/swap/gen/ic_sns_swap.pb.v1.rs - Lifecycle
+export enum SwapLifecycle {
+  Unspecified = 0,
+  Pending = 1,
+  Open = 2,
+  Committed = 3,
+  Aborted = 4,
+}
