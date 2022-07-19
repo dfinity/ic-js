69a5b9a12aab09d1ef25b4101937ce135dfa451b
peterpeterparker <david.dalbusco@outlook.com>
2022-07-19 14:57:07 +0200

    feat: expose more sns types for the swap

--- packages/sns/src/index.ts
+++ packages/sns/src/index.ts
@@ -1,6 +1,13 @@
 export type { Neuron, NeuronId } from "../candid/sns_governance";
 export type { CanisterStatusResultV2 } from "../candid/sns_root";
-export type { DerivedState, Swap } from "../candid/sns_swap";
+export type {
+  BuyerState,
+  DerivedState,
+  Init,
+  State,
+  Swap,
+  TimeWindow,
+} from "../candid/sns_swap";
 export { GovernanceCanister } from "./governance.canister";
 export { LedgerCanister } from "./ledger.canister";
 export { RootCanister } from "./root.canister";
