674f8fa177fa4ffb3be9b2f9477fc73d5cfa8142
Formatting Committer <nobody@example.com>
2022-08-04 16:47:01 +0000

    Updating formatting

--- packages/sns/src/mocks/ledger.mock.ts
+++ packages/sns/src/mocks/ledger.mock.ts
@@ -1,10 +1,12 @@
 import type { Value } from "../../candid/icrc1_ledger";
 import { SnsMetadataResponseEntries } from "../types/ledger.responses";
 
-export const tokeMetadataResponseMock: [string | SnsMetadataResponseEntries, Value][] =
-  [
-    [SnsMetadataResponseEntries.DECIMALS, { Nat: BigInt(8) }],
-    [SnsMetadataResponseEntries.NAME, { Text: "Beta Test" }],
-    [SnsMetadataResponseEntries.SYMBOL, { Text: "BTA" }],
-    [SnsMetadataResponseEntries.FEE, { Nat: BigInt(1000) }],
-  ];
+export const tokeMetadataResponseMock: [
+  string | SnsMetadataResponseEntries,
+  Value
+][] = [
+  [SnsMetadataResponseEntries.DECIMALS, { Nat: BigInt(8) }],
+  [SnsMetadataResponseEntries.NAME, { Text: "Beta Test" }],
+  [SnsMetadataResponseEntries.SYMBOL, { Text: "BTA" }],
+  [SnsMetadataResponseEntries.FEE, { Nat: BigInt(1000) }],
+];
