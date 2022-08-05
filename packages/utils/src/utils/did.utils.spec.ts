4ef6faae212430613ef073e78bb9b77db839d9aa
mstrasinskis <98811342+mstrasinskis@users.noreply.github.com>
2022-08-05 17:50:04 +0200

    feat: fromDefinedNullable (#171)
    # Motivation
    
    Add `fromDefinedNullable` util.
    
    # Changes
    
    - ++`fromDefinedNullable`
    
    # Tests
    
    - /did.utils/fromDefinedNullable

--- packages/utils/src/utils/did.utils.spec.ts
+++ packages/utils/src/utils/did.utils.spec.ts
@@ -1,4 +1,5 @@
-import { fromNullable, toNullable } from "./did.utils";
+import { NullishError } from "./asserts.utils";
+import { fromDefinedNullable, fromNullable, toNullable } from "./did.utils";
 
 describe("did-utils", () => {
   it("should convert from empty array to undefined", () => {
@@ -17,5 +18,16 @@ describe("did-utils", () => {
   it("should convert object to array", () => {
     const test = { test: "1" };
     expect(toNullable(test)).toEqual([test]);
+  });
+
+  describe("fromDefinedNullable", () => {
+    it("should convert from array to object", () => {
+      const test = { test: "1" };
+      expect(fromDefinedNullable([{ test: "1" }])).toEqual(test);
+    });
+
+    it("should throw on undefined", () => {
+      expect(() => fromDefinedNullable([])).toThrow(NullishError);
+    });
   });
 });
--- packages/utils/src/utils/did.utils.ts
+++ packages/utils/src/utils/did.utils.ts
@@ -1,7 +1,17 @@
+import { assertNonNullish } from "./asserts.utils";
+
 export const toNullable = <T>(value?: T): [] | [T] => {
   return value ? [value] : [];
 };
 
 export const fromNullable = <T>(value: [] | [T]): T | undefined => {
   return value?.[0];
+};
+
+export const fromDefinedNullable = <T>(value: [] | [T]): T => {
+  const result = fromNullable(value);
+
+  assertNonNullish(result);
+
+  return result;
 };
