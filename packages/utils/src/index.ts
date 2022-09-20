09fe88f8ec933a98027a8feb072f30bb01e3fc29
David Dal Busco <david.dalbusco@dfinity.org>
2022-09-20 07:37:23 +0200

    refactor: plural file name

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,6 @@
 export type { CanisterOptions } from "./types/canister.options";
 export * from "./utils/actor.utils";
 export * from "./utils/agent.utils";
-export * from "./utils/array.utils";
+export * from "./utils/arrays.utils";
 export * from "./utils/asserts.utils";
 export * from "./utils/did.utils";
