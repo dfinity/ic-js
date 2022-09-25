3809017636c6454b8a0a7440d96fdef44bca7e59
David Dal Busco <david.dalbusco@dfinity.org>
2022-09-25 20:02:03 +0200

    test: cmc and utils

--- packages/cmc/src/index.ts
+++ packages/cmc/src/index.ts
@@ -1,3 +1,3 @@
-export { CMCCanister } from './cmc.canister';
-export * from './cmc.errors';
-export * from './cmc.utils';
+export { CMCCanister } from "./cmc.canister";
+export * from "./cmc.errors";
+export * from "./cmc.utils";
