bae0035591d26812a3a69d09a7901826c08325bd
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2022-09-26 11:16:01 +0200

    GIX: Sns Account Encode and Decode utils (#201)
    # Motivation
    
    User sees the string representation of the SNS Accounts.
    
    # Changes
    
    * New ledger utils to encode and decode the sns accounts.
    
    # Tests
    
    * Test new ledger utils.

---
+++ packages/sns/src/utils/ledger.utils.ts
@@ -0,0 +1,99 @@
+import { Principal } from "@dfinity/principal";
+import type { SnsAccount } from "../types/ledger.responses";
+
+// https://github.com/dfinity/ICRC-1/pull/55/files#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5R236
+const EXTRA_BYTES = parseInt("FF", 16);
+const MAX_ACCOUNT_BYTES_LENGTH = 32;
+
+/**
+ * Removes leading zeros from a Uint8Array
+ *
+ * @param bytes Uint8Array
+ * @returns Uint8Array
+ */
+const shrink = (bytes: Uint8Array): Uint8Array => {
+  const shrinked = Array.from(bytes);
+  while (shrinked[0] === 0) {
+    shrinked.shift();
+  }
+  return Uint8Array.from(shrinked);
+};
+
+/**
+ * Encodes an SNS account into a string.
+ * Formatting Reference: https://github.com/dfinity/ICRC-1/pull/55/files#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5R238
+ *
+ * @param snsAccount { owner: Principal, subaccount?: Uint8Array }
+ * @returns string
+ */
+export const encodeSnsAccount = ({ owner, subaccount }: SnsAccount): string => {
+  if (subaccount === undefined) {
+    return owner.toText();
+  }
+
+  const subaccountBytes = shrink(subaccount);
+
+  if (subaccountBytes.length === 0) {
+    return owner.toText();
+  }
+
+  const bytes = Uint8Array.from([
+    ...owner.toUint8Array(),
+    ...subaccountBytes,
+    subaccountBytes.length,
+    EXTRA_BYTES,
+  ]);
+
+  return Principal.fromUint8Array(bytes).toText();
+};
+
+/**
+ * Decodes a string into an SNS account.
+ * Formatting Reference: https://github.com/dfinity/ICRC-1/pull/55/files#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5R268
+ *
+ * @param snsAccountString string
+ * @throws Error if the string is not a valid SNS account
+ * @returns snsAccount { owner: Principal, subaccount?: Uint8Array }
+ */
+export const decodeSnsAccount = (accountString: string): SnsAccount => {
+  const principal = Principal.fromText(accountString);
+
+  const [ff, nonZeroLength, ...restReversed] = principal
+    .toUint8Array()
+    .reverse();
+
+  if (ff !== EXTRA_BYTES) {
+    return {
+      owner: Principal.fromText(accountString),
+    };
+  }
+
+  if (
+    nonZeroLength > MAX_ACCOUNT_BYTES_LENGTH ||
+    nonZeroLength === 0 ||
+    nonZeroLength === undefined
+  ) {
+    throw new Error("Invalid account string");
+  }
+
+  const subaccountBytesReversed = restReversed.slice(0, nonZeroLength);
+  if (
+    subaccountBytesReversed[0] === 0 ||
+    subaccountBytesReversed.length !== nonZeroLength
+  ) {
+    throw new Error("Invalid account string");
+  }
+  while (subaccountBytesReversed.length < MAX_ACCOUNT_BYTES_LENGTH) {
+    subaccountBytesReversed.push(0);
+  }
+  const subaccount = Uint8Array.from(subaccountBytesReversed.reverse());
+
+  const principalBytes = restReversed
+    .reverse()
+    .filter((_, i) => i < restReversed.length - nonZeroLength);
+
+  return {
+    owner: Principal.fromUint8Array(Uint8Array.from(principalBytes)),
+    subaccount,
+  };
+};
