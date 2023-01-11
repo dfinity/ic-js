911bf7a84b72f8ddefaffc543ecc52777a8d8362
Llorenç <llorenc.muntaner@dfinity.org>
2023-01-11 11:03:16 +0100

    Test Vectors: SNS add neuron permissions

--- scripts/test-vectors/20220817-index.ts
+++ scripts/test-vectors/20220817-index.ts
@@ -2,35 +2,39 @@
 // https://github.com/Zondax/ledger-icp/issues/166
 // We'll keep each script for reproducibility.
 import { IDL } from "@dfinity/candid";
-import { NeuronId } from "../../packages/nns/src";
-import { fromListNeurons } from "../../packages/nns/src/canisters/governance/request.converters";
-import { ListNeuronsFn } from "./governance.idl";
+import { toLeaveCommunityFundRequest } from "@dfinity/nns/src/canisters/governance/request.converters";
+import { ManageNeuronFn } from "./governance.idl";
 import { createBlob, writeToJson } from "./utils";
 
 const mockNeuronId = BigInt(15374508381553347371);
 const mockNeuronId2 = BigInt(8836564053576662908);
 
-const createListNeurons = (neuronIds?: NeuronId[]) => {
-  const rawRequestBody = fromListNeurons(neuronIds);
-
+const createCandidLeaveCF = (neuronId: bigint) => {
+  const rawRequestBody = toLeaveCommunityFundRequest(neuronId);
   return {
     blob_candid: createBlob({
-      arg: IDL.encode(ListNeuronsFn.argTypes, [rawRequestBody]),
-      methodName: "list_neurons",
+      arg: IDL.encode(ManageNeuronFn.argTypes, [rawRequestBody]),
+      methodName: "manage_neuron",
     }),
-    name: "List Neurons",
+    name: "Leave Community Fund",
     candid_request: rawRequestBody,
-    output: ["0 | Transaction type : List Own Neurons"],
+    output: [
+      "0 | Transaction type : Leave Community Fund",
+      `1 | Neuron ID: ${neuronId}`,
+    ],
   };
 };
 
 const main = () => {
   try {
-    const vectors = [createListNeurons()];
+    const vectors = [
+      createCandidLeaveCF(mockNeuronId),
+      createCandidLeaveCF(mockNeuronId2),
+    ];
 
     writeToJson({
       data: vectors,
-      fileName: "test-vectors.json",
+      fileName: "test-vectors-20220817.json",
     });
     console.log("File created successfully");
   } catch (error) {
