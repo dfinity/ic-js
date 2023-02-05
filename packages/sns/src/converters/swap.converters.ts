164dc4151293c16367f7d5895d7d635797be9480
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-02-05 21:25:24 +0100

    refactor: neuronId -> subaccount

--- packages/sns/src/converters/swap.converters.ts
+++ packages/sns/src/converters/swap.converters.ts
@@ -1,9 +1,14 @@
 import type { NewSaleTicketRequest } from "../../candid/sns_swap";
 import type { NewSaleTicketParams } from "../types/swap.params";
 
+// TOOD: what is a subaccount type? [Uint8Array];?
+// type NewSaleTicketRequest = record {
+//   subaccount : opt vec nat8;
+//   amount_icp_e8s : nat64;
+// };
 // Helper for building `NewSaleTicketRequest` structure
 export const toNewSaleTicketRequest = ({
-  neuronId: { id },
+  subaccount: { id },
   amount_icp_e8s,
 }: NewSaleTicketParams): NewSaleTicketRequest => ({
   subaccount: [id],
