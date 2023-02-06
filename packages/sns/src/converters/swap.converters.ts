453306b65897fc670c7a56df6a5d180e9627dc52
mstrasinskis <maksims.strasinskis@dfinity.org>
2023-02-06 16:13:12 +0100

    refactor: NewSaleTicketParams

--- packages/sns/src/converters/swap.converters.ts
+++ packages/sns/src/converters/swap.converters.ts
@@ -1,16 +1,11 @@
 import type { NewSaleTicketRequest } from "../../candid/sns_swap";
 import type { NewSaleTicketParams } from "../types/swap.params";
 
-// TOOD: what is a subaccount type? [Uint8Array];?
-// type NewSaleTicketRequest = record {
-//   subaccount : opt vec nat8;
-//   amount_icp_e8s : nat64;
-// };
 // Helper for building `NewSaleTicketRequest` structure
 export const toNewSaleTicketRequest = ({
-  subaccount: { id },
+  subaccount,
   amount_icp_e8s,
 }: NewSaleTicketParams): NewSaleTicketRequest => ({
-  subaccount: [id],
+  subaccount: subaccount === undefined ? [] : [subaccount],
   amount_icp_e8s,
 });
--- packages/sns/src/types/swap.params.ts
+++ packages/sns/src/types/swap.params.ts
@@ -1,10 +1,9 @@
-import { NeuronId } from "../../candid/sns_governance";
 import type { E8s } from "./common";
 
 /**
  * The parameters to create a sale ticket (payment flow)
  */
 export interface NewSaleTicketParams {
-  subaccount: NeuronId;
+  subaccount?: Uint8Array;
   amount_icp_e8s: E8s;
 }
