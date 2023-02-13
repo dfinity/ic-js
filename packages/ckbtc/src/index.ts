9528b9ae1fefc5d1a54421c31718a3046c0bd499
David Dal Busco <david.dalbusco@dfinity.org>
2023-02-13 08:44:39 +0100

    feat: export result types

--- packages/ckbtc/src/index.ts
+++ packages/ckbtc/src/index.ts
@@ -1,3 +1,7 @@
+export type {
+  UpdateBalanceError as CkBTCUpdateBalanceError,
+  UpdateBalanceResult as CkBTCUpdateBalanceResult,
+} from "../candid/minter";
 export { CkBTCMinterCanister } from "./minter.canister";
 export * from "./types/minter.params";
 export * from "./types/minter.responses";
