3d92acd5ebf3307ea934838830fb0aea0dd4581c
David Dal Busco <david.dalbusco@dfinity.org>
2023-02-13 09:15:11 +0100

    feat: remove prefix in types

--- packages/ckbtc/src/index.ts
+++ packages/ckbtc/src/index.ts
@@ -1,7 +1,4 @@
-export type {
-  UpdateBalanceError as CkBTCUpdateBalanceError,
-  UpdateBalanceResult as CkBTCUpdateBalanceResult,
-} from "../candid/minter";
+export type { UpdateBalanceError, UpdateBalanceResult } from "../candid/minter";
 export { CkBTCMinterCanister } from "./minter.canister";
 export * from "./types/minter.params";
 export * from "./types/minter.responses";
