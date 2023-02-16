43bbdd0e2f50c44ae9ce75987a26c46b16af2550
Llorenç <llorenc.muntaner@dfinity.org>
2023-02-16 08:12:30 +0100

    Chore: Fix test

--- packages/sns/src/swap.canister.spec.ts
+++ packages/sns/src/swap.canister.spec.ts
@@ -214,7 +214,7 @@ describe("Swap canister", () => {
       canisterId: swapCanisterIdMock,
       certifiedServiceOverride: service,
     });
-    const res = await canister.getDerivedState({});
+    const res = await canister.getSaleParameters({});
     expect(res).toEqual(testResponse);
   });
 
