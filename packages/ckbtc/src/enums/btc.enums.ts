a89d66a6d369daeeb7237722c233fafc337034f7
David Dal Busco <david.dalbusco@dfinity.org>
2023-03-06 15:27:47 +0100

    docs: https://github.com/dfinity/ic/blob/a8da3aa23dc6f8f4708cb0cb8edce84c5bd8f225/rs/bitcoin/ckbtc/minter/src/address.rs#L18

--- packages/ckbtc/src/enums/btc.enums.ts
+++ packages/ckbtc/src/enums/btc.enums.ts
@@ -6,6 +6,7 @@ export enum BtcNetwork {
   Testnet,
 }
 
+// See https://github.com/dfinity/ic/blob/a8da3aa23dc6f8f4708cb0cb8edce84c5bd8f225/rs/bitcoin/ckbtc/minter/src/address.rs#L18
 export enum BtcAddressType {
   P2wpkhV0,
   P2pkh,
