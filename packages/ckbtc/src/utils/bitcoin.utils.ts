8cb9b2c6d68e13052b15599bb38afa6a3fa56c20
David Dal Busco <david.dalbusco@dfinity.org>
2023-03-06 09:41:12 +0100

    feat: base58

--- packages/ckbtc/src/utils/bitcoin.utils.ts
+++ packages/ckbtc/src/utils/bitcoin.utils.ts
@@ -1,6 +1,7 @@
 import { isNullish } from "@dfinity/utils/src";
+import { base58_to_binary } from "base58-js";
 import { bech32, bech32m, type Decoded } from "bech32";
-import { base58_to_binary } from 'base58-js';
+import { sha256 } from "js-sha256";
 
 export enum BtcNetwork {
   Mainnet,
@@ -13,6 +14,31 @@ export enum BitcoinAddressType {
   P2pkh,
   P2sh,
 }
+
+const Base58AddressTypes: Record<
+  number,
+  { type: BitcoinAddressType; network: BtcNetwork }
+> = {
+  0x00: {
+    type: BitcoinAddressType.P2pkh,
+    network: BtcNetwork.Mainnet,
+  },
+
+  0x6f: {
+    type: BitcoinAddressType.P2pkh,
+    network: BtcNetwork.Testnet,
+  },
+
+  0x05: {
+    type: BitcoinAddressType.P2sh,
+    network: BtcNetwork.Mainnet,
+  },
+
+  0xc4: {
+    type: BitcoinAddressType.P2sh,
+    network: BtcNetwork.Testnet,
+  },
+};
 
 export interface BitcoinAddress {
   address: string;
@@ -27,17 +53,56 @@ const parseBase58Address = ({
     try {
       return base58_to_binary(address);
     } catch (error) {
-      throw new Error('Invalid Address');
+      throw new Error("Invalid Address");
     }
-  }
+  };
 
   const decoded: Uint8Array = decodeBase58(address);
 
-  if (decoded.length !== 25) {
-    throw new Error('MalformedAddress');
+  const { length } = decoded;
+
+  if (length !== 25) {
+    throw new Error("MalformedAddress");
   }
 
+  const validateBase58Checksum = (decoded: Uint8Array) => {
+    const expectedChecksum = decoded.slice(length - 4, length);
+    const body = decoded.slice(0, length - 4);
 
+    const bodyHash = sha256.create();
+    bodyHash.update(body);
+
+    const checksumHash = sha256.create();
+    checksumHash.update(bodyHash.digest());
+
+    const checksum = checksumHash.array().slice(0, 4);
+
+    if (
+        expectedChecksum.some(
+            (value: number, index: number) => value !== checksum[index]
+        )
+    ) {
+      throw new Error("MalformedAddress");
+    }
+  }
+
+  validateBase58Checksum(decoded);
+
+  const version = decoded[0];
+
+  const versionNetwork = Base58AddressTypes[version];
+
+  if (isNullish(versionNetwork)) {
+    throw new Error("UnsupportedAddressType");
+  }
+
+  const { type, network: parsedNetwork } = versionNetwork;
+
+  if (parsedNetwork !== network) {
+    throw new Error("WrongNetwork");
+  }
+
+  return type;
 };
 
 const parseBip173Address = ({
@@ -93,6 +158,16 @@ const parseBip173Address = ({
   return BitcoinAddressType.P2wpkhV0;
 };
 
+/**
+ * Parse a Bitcoin address.
+ *
+ * Parse implementation follows strategy implemented in [Minter canister](https://github.com/dfinity/ic/blob/a8da3aa23dc6f8f4708cb0cb8edce84c5bd8f225/rs/bitcoin/ckbtc/minter/src/address.rs#L54).
+ * JavaScript code inspired by [bitcoin-address-validation](https://github.com/ruigomeseu/bitcoin-address-validation).
+ *
+ * @param {BitcoinAddress} params The Bitcoin address and network to parse
+ * @param {string} params.address
+ * @param {BtcNetwork} params.network Optional. Default BtcNetwork.Mainnet
+ */
 export const parseBitcoinAddress = ({
   address,
   network = BtcNetwork.Mainnet,
