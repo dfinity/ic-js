536e67fa4937e86e72d9275a301ba2ce1580e2c2
Formatting Committer <nobody@example.com>
2023-03-06 09:44:54 +0000

    Updating formatting

--- packages/ckbtc/src/base58.d.ts
+++ packages/ckbtc/src/base58.d.ts
@@ -1,3 +1,3 @@
-declare module 'base58-js' {
-    const base58_to_binary: (address: string) => Uint8Array;
+declare module "base58-js" {
+  const base58_to_binary: (address: string) => Uint8Array;
 }
