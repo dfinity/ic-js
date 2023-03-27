568a9b193b26bcc543e1993ba474692951ec4e6e
David Dal Busco <david.dalbusco@dfinity.org>
2023-03-27 11:00:36 +0200

    test: crc

--- packages/utils/src/utils/crc.utils.spec.ts
+++ packages/utils/src/utils/crc.utils.spec.ts
@@ -1,9 +1,15 @@
-import { getCrc32 } from "./getCrc";
+import { bigEndianCrc32 } from "./crc.utils";
 
-describe("crc", () => {
-  it("works", () => {
-    expect(getCrc32(new Uint8Array([]))).toBe(0);
-    expect(getCrc32(new Uint8Array([1, 2, 3]))).toBe(0x55bc801d);
-    expect(getCrc32(new Uint8Array([100, 99, 98, 1, 2, 3]))).toBe(0xc7e787f5);
+describe("crc-utils", () => {
+  it("should convert array to big endian crc32", () => {
+    expect(bigEndianCrc32(new Uint8Array([]))).toEqual(
+      Uint8Array.from([0, 0, 0, 0])
+    );
+    expect(bigEndianCrc32(new Uint8Array([1, 2, 3]))).toEqual(
+      Uint8Array.from([0x55, 0xbc, 0x80, 0x1d])
+    );
+    expect(bigEndianCrc32(new Uint8Array([100, 99, 98, 1, 2, 3]))).toEqual(
+      Uint8Array.from([0xc7, 0xe7, 0x87, 0xf5])
+    );
   });
 });
