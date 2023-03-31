c5df071f33e1bc1610a89d6093ad83f53fe94d92
Llorenç <llorenc.muntaner@dfinity.org>
2023-03-31 09:12:16 +0200

    GIX-1220: CR changes

--- packages/sns/src/converters/governance.converters.spec.ts
+++ packages/sns/src/converters/governance.converters.spec.ts
@@ -1,5 +1,8 @@
 import { Principal } from "@dfinity/principal";
-import type { Action as ActionCandid } from "../../candid/sns_governance";
+import type {
+  Action as ActionCandid,
+  DefaultFollowees,
+} from "../../candid/sns_governance";
 import type { Action } from "../types/actions";
 import { toActionOptional } from "./governance.converters";
 
@@ -8,90 +11,120 @@ describe("governance converters", () => {
     const mockPrincipal = Principal.fromText("aaaaa-aa");
 
     it("converts ManageNervousSystemParameters action", () => {
+      const default_followees: DefaultFollowees = {
+        followees: [[BigInt(20), { followees: [{ id: new Uint8Array(0) }] }]],
+      };
+      const max_dissolve_delay_seconds = BigInt(21);
+      const max_dissolve_delay_bonus_percentage = BigInt(22);
+      const max_followees_per_function = BigInt(23);
+      const neuron_claimer_permissions = { permissions: new Int32Array() };
+      const neuron_minimum_stake_e8s = BigInt(24);
+      const max_neuron_age_for_age_bonus = BigInt(25);
+      const initial_voting_period_seconds = BigInt(26);
+      const neuron_minimum_dissolve_delay_to_vote_seconds = BigInt(27);
+      const reject_cost_e8s = BigInt(28);
+      const max_proposals_to_keep_per_action = 2;
+      const max_number_of_neurons = BigInt(29);
+      const transaction_fee_e8s = BigInt(30);
+      const max_age_bonus_percentage = BigInt(31);
+      const neuron_grantable_permissions = { permissions: new Int32Array() };
+      const final_reward_rate_basis_points = BigInt(32);
+      const initial_reward_rate_basis_points = BigInt(33);
+      const reward_rate_transition_duration_seconds = BigInt(34);
+      const round_duration_seconds = BigInt(35);
+      const max_number_of_principals_per_neuron = BigInt(2);
+
       const action: ActionCandid = {
         ManageNervousSystemParameters: {
-          default_followees: [
-            {
-              followees: [
-                [BigInt(2), { followees: [{ id: new Uint8Array(0) }] }],
-              ],
-            },
+          default_followees: [default_followees],
+          max_dissolve_delay_seconds: [max_dissolve_delay_seconds],
+          max_dissolve_delay_bonus_percentage: [
+            max_dissolve_delay_bonus_percentage,
           ],
-          max_dissolve_delay_seconds: [BigInt(2)],
-          max_dissolve_delay_bonus_percentage: [BigInt(2)],
-          max_followees_per_function: [BigInt(2)],
-          neuron_claimer_permissions: [{ permissions: new Int32Array() }],
-          neuron_minimum_stake_e8s: [BigInt(2)],
-          max_neuron_age_for_age_bonus: [BigInt(2)],
-          initial_voting_period_seconds: [BigInt(2)],
-          neuron_minimum_dissolve_delay_to_vote_seconds: [BigInt(2)],
-          reject_cost_e8s: [BigInt(2)],
-          max_proposals_to_keep_per_action: [2],
+          max_followees_per_function: [max_followees_per_function],
+          neuron_claimer_permissions: [neuron_claimer_permissions],
+          neuron_minimum_stake_e8s: [neuron_minimum_stake_e8s],
+          max_neuron_age_for_age_bonus: [max_neuron_age_for_age_bonus],
+          initial_voting_period_seconds: [initial_voting_period_seconds],
+          neuron_minimum_dissolve_delay_to_vote_seconds: [
+            neuron_minimum_dissolve_delay_to_vote_seconds,
+          ],
+          reject_cost_e8s: [reject_cost_e8s],
+          max_proposals_to_keep_per_action: [max_proposals_to_keep_per_action],
           wait_for_quiet_deadline_increase_seconds: [],
-          max_number_of_neurons: [BigInt(2)],
-          transaction_fee_e8s: [BigInt(2)],
+          max_number_of_neurons: [max_number_of_neurons],
+          transaction_fee_e8s: [transaction_fee_e8s],
           max_number_of_proposals_with_ballots: [],
-          max_age_bonus_percentage: [BigInt(2)],
-          neuron_grantable_permissions: [{ permissions: new Int32Array() }],
+          max_age_bonus_percentage: [max_age_bonus_percentage],
+          neuron_grantable_permissions: [neuron_grantable_permissions],
           voting_rewards_parameters: [
             {
-              final_reward_rate_basis_points: [BigInt(3)],
-              initial_reward_rate_basis_points: [BigInt(3)],
-              reward_rate_transition_duration_seconds: [BigInt(3)],
-              round_duration_seconds: [BigInt(3)],
+              final_reward_rate_basis_points: [final_reward_rate_basis_points],
+              initial_reward_rate_basis_points: [
+                initial_reward_rate_basis_points,
+              ],
+              reward_rate_transition_duration_seconds: [
+                reward_rate_transition_duration_seconds,
+              ],
+              round_duration_seconds: [round_duration_seconds],
             },
           ],
-          max_number_of_principals_per_neuron: [BigInt(2)],
+          max_number_of_principals_per_neuron: [
+            max_number_of_principals_per_neuron,
+          ],
         },
       };
       const expectedAction: Action = {
         ManageNervousSystemParameters: {
-          default_followees: {
-            followees: [
-              [BigInt(2), { followees: [{ id: new Uint8Array(0) }] }],
-            ],
-          },
-          max_dissolve_delay_seconds: BigInt(2),
-          max_dissolve_delay_bonus_percentage: BigInt(2),
-          max_followees_per_function: BigInt(2),
-          neuron_claimer_permissions: { permissions: new Int32Array() },
-          neuron_minimum_stake_e8s: BigInt(2),
-          max_neuron_age_for_age_bonus: BigInt(2),
-          initial_voting_period_seconds: BigInt(2),
-          neuron_minimum_dissolve_delay_to_vote_seconds: BigInt(2),
-          reject_cost_e8s: BigInt(2),
+          default_followees,
+          max_dissolve_delay_seconds,
+          max_dissolve_delay_bonus_percentage,
+          max_followees_per_function,
+          neuron_claimer_permissions,
+          neuron_minimum_stake_e8s,
+          max_neuron_age_for_age_bonus,
+          initial_voting_period_seconds,
+          neuron_minimum_dissolve_delay_to_vote_seconds,
+          reject_cost_e8s,
           max_proposals_to_keep_per_action: 2,
           wait_for_quiet_deadline_increase_seconds: undefined,
-          max_number_of_neurons: BigInt(2),
-          transaction_fee_e8s: BigInt(2),
+          max_number_of_neurons,
+          transaction_fee_e8s,
           max_number_of_proposals_with_ballots: undefined,
-          max_age_bonus_percentage: BigInt(2),
-          neuron_grantable_permissions: { permissions: new Int32Array() },
+          max_age_bonus_percentage,
+          neuron_grantable_permissions,
           voting_rewards_parameters: {
-            final_reward_rate_basis_points: BigInt(3),
-            initial_reward_rate_basis_points: BigInt(3),
-            reward_rate_transition_duration_seconds: BigInt(3),
-            round_duration_seconds: BigInt(3),
+            final_reward_rate_basis_points,
+            initial_reward_rate_basis_points,
+            reward_rate_transition_duration_seconds,
+            round_duration_seconds,
           },
-          max_number_of_principals_per_neuron: BigInt(2),
+          max_number_of_principals_per_neuron,
         },
       };
       expect(toActionOptional(action)).toEqual(expectedAction);
     });
 
     it("converts AddGenericNervousSystemFunction action", () => {
+      const id = BigInt(3);
+      const name = "test function";
+      const description = "test description";
+      const validator_canister_id = mockPrincipal;
+      const target_canister_id = mockPrincipal;
+      const validator_method_name = "validator_method_name";
+      const target_method_name = "target_method_name";
       const action: ActionCandid = {
         AddGenericNervousSystemFunction: {
-          id: BigInt(3),
-          name: "test function",
-          description: ["test description"],
+          id,
+          name,
+          description: [description],
           function_type: [
             {
               GenericNervousSystemFunction: {
-                validator_canister_id: [],
-                target_canister_id: [mockPrincipal],
-                validator_method_name: ["validator_method_name"],
-                target_method_name: ["target_method_name"],
+                validator_canister_id: [validator_canister_id],
+                target_canister_id: [target_canister_id],
+                validator_method_name: [validator_method_name],
+                target_method_name: [target_method_name],
               },
             },
           ],
@@ -99,15 +132,15 @@ describe("governance converters", () => {
       };
       const expectedAction: Action = {
         AddGenericNervousSystemFunction: {
-          id: BigInt(3),
-          name: "test function",
-          description: "test description",
+          id,
+          name,
+          description,
           function_type: {
             GenericNervousSystemFunction: {
-              validator_canister_id: undefined,
-              target_canister_id: mockPrincipal,
-              validator_method_name: "validator_method_name",
-              target_method_name: "target_method_name",
+              validator_canister_id,
+              target_canister_id,
+              validator_method_name,
+              target_method_name,
             },
           },
         },
@@ -119,20 +152,14 @@ describe("governance converters", () => {
       const action: ActionCandid = {
         RemoveGenericNervousSystemFunction: BigInt(3),
       };
-      const expectedAction: Action = {
-        RemoveGenericNervousSystemFunction: BigInt(3),
-      };
-      expect(toActionOptional(action)).toEqual(expectedAction);
+      expect(toActionOptional(action)).toEqual(action);
     });
 
     it("converts UpgradeSnsToNextVersion action", () => {
       const action: ActionCandid = {
         UpgradeSnsToNextVersion: {},
       };
-      const expectedAction: Action = {
-        UpgradeSnsToNextVersion: {},
-      };
-      expect(toActionOptional(action)).toEqual(expectedAction);
+      expect(toActionOptional(action)).toEqual(action);
     });
 
     it("converts RegisterDappCanisters action", () => {
@@ -141,48 +168,49 @@ describe("governance converters", () => {
           canister_ids: [mockPrincipal],
         },
       };
-      const expectedAction: Action = {
-        RegisterDappCanisters: {
-          canister_ids: [mockPrincipal],
-        },
-      };
-      expect(toActionOptional(action)).toEqual(expectedAction);
+      expect(toActionOptional(action)).toEqual(action);
     });
 
     it("converts TransferSnsTreasuryFunds action", () => {
+      const from_treasury = 3;
+      const to_principal = mockPrincipal;
+      const memo = BigInt(3);
+      const amount_e8s = BigInt(3);
       const action: ActionCandid = {
         TransferSnsTreasuryFunds: {
-          from_treasury: 3,
-          to_principal: [mockPrincipal],
+          from_treasury,
+          to_principal: [to_principal],
           to_subaccount: [],
-          memo: [BigInt(3)],
-          amount_e8s: BigInt(3),
+          memo: [memo],
+          amount_e8s,
         },
       };
       const expectedAction: Action = {
         TransferSnsTreasuryFunds: {
-          from_treasury: 3,
-          to_principal: mockPrincipal,
+          from_treasury,
+          to_principal,
           to_subaccount: undefined,
-          memo: BigInt(3),
-          amount_e8s: BigInt(3),
+          memo,
+          amount_e8s,
         },
       };
       expect(toActionOptional(action)).toEqual(expectedAction);
     });
 
     it("converts UpgradeSnsControlledCanister action", () => {
+      const new_canister_wasm = new Uint8Array();
+      const canister_id = mockPrincipal;
       const action: ActionCandid = {
         UpgradeSnsControlledCanister: {
-          new_canister_wasm: new Uint8Array(),
-          canister_id: [mockPrincipal],
+          new_canister_wasm,
+          canister_id: [canister_id],
           canister_upgrade_arg: [],
         },
       };
       const expectedAction: Action = {
         UpgradeSnsControlledCanister: {
           new_canister_wasm: new Uint8Array(),
-          canister_id: mockPrincipal,
+          canister_id,
           canister_upgrade_arg: undefined,
         },
       };
@@ -196,39 +224,32 @@ describe("governance converters", () => {
           new_controllers: [mockPrincipal],
         },
       };
-      const expectedAction: Action = {
-        DeregisterDappCanisters: {
-          canister_ids: [mockPrincipal],
-          new_controllers: [mockPrincipal],
-        },
-      };
-      expect(toActionOptional(action)).toEqual(expectedAction);
+      expect(toActionOptional(action)).toEqual(action);
     });
 
     it("converts Unspecified action", () => {
       const action: ActionCandid = {
         Unspecified: {},
       };
-      const expectedAction: Action = {
-        Unspecified: {},
-      };
-      expect(toActionOptional(action)).toEqual(expectedAction);
+      expect(toActionOptional(action)).toEqual(action);
     });
 
     it("converts ManageSnsMetadata action", () => {
+      const url = "https://example.com";
+      const name = "New name";
       const action: ActionCandid = {
         ManageSnsMetadata: {
-          url: ["https://example.com"],
+          url: [url],
           logo: [],
-          name: ["New name"],
+          name: [name],
           description: [],
         },
       };
       const expectedAction: Action = {
         ManageSnsMetadata: {
-          url: "https://example.com",
+          url,
           logo: undefined,
-          name: "New name",
+          name,
           description: undefined,
         },
       };
@@ -242,23 +263,14 @@ describe("governance converters", () => {
           payload: new Uint8Array(),
         },
       };
-      const expectedAction: Action = {
-        ExecuteGenericNervousSystemFunction: {
-          function_id: BigInt(3),
-          payload: new Uint8Array(),
-        },
-      };
-      expect(toActionOptional(action)).toEqual(expectedAction);
+      expect(toActionOptional(action)).toEqual(action);
     });
 
     it("converts Motion action", () => {
       const action: ActionCandid = {
         Motion: { motion_text: "test motion" },
       };
-      const expectedAction: Action = {
-        Motion: { motion_text: "test motion" },
-      };
-      expect(toActionOptional(action)).toEqual(expectedAction);
+      expect(toActionOptional(action)).toEqual(action);
     });
   });
 });
