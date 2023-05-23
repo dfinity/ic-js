5e4032f88b82ba5115fd8910b8faf098832e6357
David Dal Busco <david.dalbusco@dfinity.org>
2023-05-23 14:01:55 +0200

    build: proto path

--- scripts/update_proto.sh
+++ scripts/update_proto.sh
@@ -2,21 +2,21 @@
 # A script for generating the JS of proto files.
 protoc \
   --plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts" \
-  --ts_out="./packages/nns/" \
-  --js_out="import_style=commonjs,binary:./packages/nns/" \
-  --proto_path="./packages/nns/" \
+  --ts_out="./packages/nns-proto/" \
+  --js_out="import_style=commonjs,binary:./packages/nns-proto/" \
+  --proto_path="./packages/nns-proto/" \
   ./packages/nns-proto/proto/base_types.proto
 
 protoc \
   --plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts" \
-  --ts_out="./packages/nns/proto/" \
-  --js_out="import_style=commonjs,binary:./packages/nns/proto/" \
-  --proto_path="./packages/nns/proto" \
+  --ts_out="./packages/nns-proto/proto/" \
+  --js_out="import_style=commonjs,binary:./packages/nns-proto/proto/" \
+  --proto_path="./packages/nns-proto/proto" \
   ./packages/nns-proto/proto/ledger.proto
 
 protoc \
   --plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts" \
-  --ts_out="./packages/nns/proto/" \
-  --js_out="import_style=commonjs,binary:./packages/nns/proto/" \
-  --proto_path="./packages/nns/proto" \
+  --ts_out="./packages/nns-proto/proto/" \
+  --js_out="import_style=commonjs,binary:./packages/nns-proto/proto/" \
+  --proto_path="./packages/nns-proto/proto" \
   ./packages/nns-proto/proto/governance.proto
