a595ec75b989eb1709b2450ec21c6ffd1b043b4e
David Dal Busco <david.dalbusco@dfinity.org>
2023-06-21 08:06:57 +0200

    build unlock ci

--- packages/ic-management/src/index.ts
+++ packages/ic-management/src/index.ts
@@ -1,4 +1,3 @@
 export { ICManagementCanister } from "./ic-management.canister";
-export * from "./types/canister.options";
 export * from "./types/ic-management.params";
 export * from "./types/ic-management.responses";
