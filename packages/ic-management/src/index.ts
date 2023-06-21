6b3fcc5f762a1371b791cdeba3c2a14622be1bc5
David Dal Busco <david.dalbusco@dfinity.org>
2023-06-21 07:48:07 +0200

    feat: export ic-mgmt options type

--- packages/ic-management/src/index.ts
+++ packages/ic-management/src/index.ts
@@ -1,3 +1,4 @@
 export { ICManagementCanister } from "./ic-management.canister";
 export * from "./types/ic-management.params";
 export * from "./types/ic-management.responses";
+export * from "./types/canister.options";
