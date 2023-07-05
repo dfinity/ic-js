73ec76952edaf62bafa20cb5f5b30fcb2fba23a8
David Dal Busco <david.dalbusco@dfinity.org>
2023-07-05 14:59:58 +0200

    feat: get balance for Index canister as well

--- packages/ledger/src/index.canister.ts
+++ packages/ledger/src/index.canister.ts
@@ -1,16 +1,19 @@
-import { Canister, createServices } from "@dfinity/utils";
+import { createServices, toNullable } from "@dfinity/utils";
 import type {
   GetTransactions,
+  Tokens,
   _SERVICE as IcrcIndexService,
 } from "../candid/icrc1_index";
 import { idlFactory as certifiedIdlFactory } from "../candid/icrc1_index.certified.idl";
 import { idlFactory } from "../candid/icrc1_index.idl";
+import { IcrcCanister } from "./canister";
 import { toGetTransactionsArgs } from "./converters/index.converters";
 import { IndexError } from "./errors/index.errors";
 import type { IcrcLedgerCanisterOptions } from "./types/canister.options";
 import type { GetAccountTransactionsParams } from "./types/index.params";
+import type { BalanceParams } from "./types/ledger.params";
 
-export class IcrcIndexCanister extends Canister<IcrcIndexService> {
+export class IcrcIndexCanister extends IcrcCanister<IcrcIndexService> {
   static create(options: IcrcLedgerCanisterOptions<IcrcIndexService>) {
     const { service, certifiedService, canisterId } =
       createServices<IcrcIndexService>({
@@ -42,4 +45,16 @@ export class IcrcIndexCanister extends Canister<IcrcIndexService> {
 
     return response.Ok;
   };
+
+  /**
+   * Returns the balance for a given account provided as owner and with optional subaccount.
+   *
+   * @param {BalanceParams} params The parameters to get the balance of an account.
+   * @returns {Promise<Tokens>} The balance of the given account.
+   */
+  balance = (params: BalanceParams): Promise<Tokens> =>
+    this.caller({ certified: params.certified }).icrc1_balance_of({
+      owner: params.owner,
+      subaccount: toNullable(params.subaccount),
+    });
 }
