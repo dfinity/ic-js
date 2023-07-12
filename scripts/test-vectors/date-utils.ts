b5d76f5b15d35f08c26f3ecf93827502a20dfcb4
Llorenç <llorenc.muntaner@dfinity.org>
2023-07-12 10:23:25 +0200

    20230712: SNS Set Dissolve Timestamp

---
+++ scripts/test-vectors/date-utils.ts
@@ -0,0 +1,60 @@
+const SECONDS_IN_MINUTE = 60;
+const SECONDS_IN_HOUR = SECONDS_IN_MINUTE * 60;
+export const SECONDS_IN_DAY = SECONDS_IN_HOUR * 24;
+// Taking into account 1/4 of leap year
+export const SECONDS_IN_YEAR = ((4 * 365 + 1) * SECONDS_IN_DAY) / 4;
+export const SECONDS_IN_MONTH = SECONDS_IN_YEAR / 12;
+
+type LabelKey = "year" | "month" | "day" | "hour" | "minute" | "second";
+type LabelInfo = {
+  labelKey: LabelKey;
+  amount: number;
+};
+const createLabel = (labelKey: LabelKey, amount: bigint): LabelInfo => ({
+  labelKey,
+  amount: Number(amount),
+});
+
+const labels: { [key: string]: string } = {
+  year: "year",
+  year_plural: "years",
+  month: "month",
+  month_plural: "months",
+  day: "day",
+  day_plural: "days",
+};
+
+/**
+ * Displays years, months and days.
+ *
+ * Uses constants for `year` and `month`:
+ * - a year = 365.25 * 24 * 60 * 60
+ * - a month = 1 year / 12
+ * - rounds up days
+ *
+ * @param seconds
+ */
+export const secondsToDissolveDelayDuration = (seconds: bigint): string => {
+  const years = seconds / BigInt(SECONDS_IN_YEAR);
+  const months = (seconds % BigInt(SECONDS_IN_YEAR)) / BigInt(SECONDS_IN_MONTH);
+  const days = BigInt(
+    Math.ceil((Number(seconds) % SECONDS_IN_MONTH) / SECONDS_IN_DAY)
+  );
+  const periods = [
+    createLabel("year", years),
+    createLabel("month", months),
+    createLabel("day", days),
+  ];
+
+  return periods
+    .filter(({ amount }) => amount > 0)
+    .map(
+      (labelInfo) =>
+        `${labelInfo.amount} ${
+          labelInfo.amount === 1
+            ? labels[labelInfo.labelKey]
+            : labels[`${labelInfo.labelKey}_plural`]
+        }`
+    )
+    .join(", ");
+};
