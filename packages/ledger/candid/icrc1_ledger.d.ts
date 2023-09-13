47b3522117d18b21be44c07ae5c8e60c27a14cb5
David Dal Busco <david.dalbusco@dfinity.org>
2023-09-13 07:22:11 +0200

    feat: ledger icrc2

--- packages/ledger/candid/icrc1_ledger.d.ts
+++ packages/ledger/candid/icrc1_ledger.d.ts
@@ -5,10 +5,58 @@ export interface Account {
   owner: Principal;
   subaccount: [] | [Subaccount];
 }
+export interface Allowance {
+  allowance: bigint;
+  expires_at: [] | [bigint];
+}
+export interface AllowanceArgs {
+  account: Account;
+  spender: Account;
+}
+export interface Approve {
+  fee: [] | [bigint];
+  from: Account;
+  memo: [] | [Uint8Array];
+  created_at_time: [] | [bigint];
+  amount: bigint;
+  expected_allowance: [] | [bigint];
+  expires_at: [] | [bigint];
+  spender: Account;
+}
+export interface ApproveArgs {
+  fee: [] | [bigint];
+  memo: [] | [Uint8Array];
+  from_subaccount: [] | [Uint8Array];
+  created_at_time: [] | [bigint];
+  amount: bigint;
+  expected_allowance: [] | [bigint];
+  expires_at: [] | [bigint];
+  spender: Account;
+}
+export type ApproveError =
+  | {
+      GenericError: { message: string; error_code: bigint };
+    }
+  | { TemporarilyUnavailable: null }
+  | { Duplicate: { duplicate_of: bigint } }
+  | { BadFee: { expected_fee: bigint } }
+  | { AllowanceChanged: { current_allowance: bigint } }
+  | { CreatedInFuture: { ledger_time: bigint } }
+  | { TooOld: null }
+  | { Expired: { ledger_time: bigint } }
+  | { InsufficientFunds: { balance: bigint } };
+export type ApproveResult = { Ok: bigint } | { Err: ApproveError };
 export type Block = Value;
 export type BlockIndex = bigint;
 export interface BlockRange {
   blocks: Array<Block>;
+}
+export interface Burn {
+  from: Account;
+  memo: [] | [Uint8Array];
+  created_at_time: [] | [bigint];
+  amount: bigint;
+  spender: [] | [Account];
 }
 export type ChangeFeeCollector = { SetTo: Account } | { Unset: null };
 export interface DataCertificate {
@@ -16,6 +64,9 @@ export interface DataCertificate {
   hash_tree: Uint8Array;
 }
 export type Duration = bigint;
+export interface FeatureFlags {
+  icrc2: boolean;
+}
 export interface GetBlocksArgs {
   start: BlockIndex;
   length: bigint;
@@ -45,22 +96,39 @@ export interface GetTransactionsResponse {
     length: bigint;
   }>;
 }
+export interface HttpRequest {
+  url: string;
+  method: string;
+  body: Uint8Array;
+  headers: Array<[string, string]>;
+}
+export interface HttpResponse {
+  body: Uint8Array;
+  headers: Array<[string, string]>;
+  status_code: number;
+}
 export interface InitArgs {
+  decimals: [] | [number];
   token_symbol: string;
   transfer_fee: bigint;
   metadata: Array<[string, MetadataValue]>;
   minting_account: Account;
   initial_balances: Array<[Account, bigint]>;
+  maximum_number_of_accounts: [] | [bigint];
+  accounts_overflow_trim_quantity: [] | [bigint];
   fee_collector_account: [] | [Account];
   archive_options: {
     num_blocks_to_archive: bigint;
+    max_transactions_per_response: [] | [bigint];
     trigger_threshold: bigint;
     max_message_size_bytes: [] | [bigint];
     cycles_for_archive_creation: [] | [bigint];
     node_max_memory_size_bytes: [] | [bigint];
     controller_id: Principal;
   };
+  max_memo_length: [] | [number];
   token_name: string;
+  feature_flags: [] | [FeatureFlags];
 }
 export type LedgerArg = { Upgrade: [] | [UpgradeArgs] } | { Init: InitArgs };
 export type Map = Array<[string, Value]>;
@@ -69,52 +137,43 @@ export type MetadataValue =
   | { Nat: bigint }
   | { Blob: Uint8Array }
   | { Text: string };
+export interface Mint {
+  to: Account;
+  memo: [] | [Uint8Array];
+  created_at_time: [] | [bigint];
+  amount: bigint;
+}
 export type QueryArchiveFn = ActorMethod<
   [GetTransactionsRequest],
   TransactionRange
 >;
 export type QueryBlockArchiveFn = ActorMethod<[GetBlocksArgs], BlockRange>;
+export interface StandardRecord {
+  url: string;
+  name: string;
+}
 export type Subaccount = Uint8Array;
 export type Timestamp = bigint;
 export type Tokens = bigint;
 export interface Transaction {
-  burn:
-    | []
-    | [
-        {
-          from: Account;
-          memo: [] | [Uint8Array];
-          created_at_time: [] | [bigint];
-          amount: bigint;
-        },
-      ];
+  burn: [] | [Burn];
   kind: string;
-  mint:
-    | []
-    | [
-        {
-          to: Account;
-          memo: [] | [Uint8Array];
-          created_at_time: [] | [bigint];
-          amount: bigint;
-        },
-      ];
+  mint: [] | [Mint];
+  approve: [] | [Approve];
   timestamp: bigint;
-  transfer:
-    | []
-    | [
-        {
-          to: Account;
-          fee: [] | [bigint];
-          from: Account;
-          memo: [] | [Uint8Array];
-          created_at_time: [] | [bigint];
-          amount: bigint;
-        },
-      ];
+  transfer: [] | [Transfer];
 }
 export interface TransactionRange {
   transactions: Array<Transaction>;
+}
+export interface Transfer {
+  to: Account;
+  fee: [] | [bigint];
+  from: Account;
+  memo: [] | [Uint8Array];
+  created_at_time: [] | [bigint];
+  amount: bigint;
+  spender: [] | [Account];
 }
 export interface TransferArg {
   to: Account;
@@ -135,15 +194,42 @@ export type TransferError =
   | { CreatedInFuture: { ledger_time: bigint } }
   | { TooOld: null }
   | { InsufficientFunds: { balance: Tokens } };
+export interface TransferFromArgs {
+  to: Account;
+  fee: [] | [Tokens];
+  spender_subaccount: [] | [Subaccount];
+  from: Account;
+  memo: [] | [Uint8Array];
+  created_at_time: [] | [Timestamp];
+  amount: Tokens;
+}
+export type TransferFromError =
+  | {
+      GenericError: { message: string; error_code: bigint };
+    }
+  | { TemporarilyUnavailable: null }
+  | { InsufficientAllowance: { allowance: Tokens } }
+  | { BadBurn: { min_burn_amount: Tokens } }
+  | { Duplicate: { duplicate_of: BlockIndex } }
+  | { BadFee: { expected_fee: Tokens } }
+  | { CreatedInFuture: { ledger_time: bigint } }
+  | { TooOld: null }
+  | { InsufficientFunds: { balance: Tokens } };
+export type TransferFromResult =
+  | { Ok: BlockIndex }
+  | { Err: TransferFromError };
 export type TransferResult = { Ok: BlockIndex } | { Err: TransferError };
 export type TxIndex = bigint;
 export interface UpgradeArgs {
   token_symbol: [] | [string];
   transfer_fee: [] | [bigint];
   metadata: [] | [Array<[string, MetadataValue]>];
+  maximum_number_of_accounts: [] | [bigint];
+  accounts_overflow_trim_quantity: [] | [bigint];
   change_fee_collector: [] | [ChangeFeeCollector];
   max_memo_length: [] | [number];
   token_name: [] | [string];
+  feature_flags: [] | [FeatureFlags];
 }
 export type Value =
   | { Int: bigint }
@@ -166,11 +252,11 @@ export interface _SERVICE {
   icrc1_metadata: ActorMethod<[], Array<[string, MetadataValue]>>;
   icrc1_minting_account: ActorMethod<[], [] | [Account]>;
   icrc1_name: ActorMethod<[], string>;
-  icrc1_supported_standards: ActorMethod<
-    [],
-    Array<{ url: string; name: string }>
-  >;
+  icrc1_supported_standards: ActorMethod<[], Array<StandardRecord>>;
   icrc1_symbol: ActorMethod<[], string>;
   icrc1_total_supply: ActorMethod<[], Tokens>;
   icrc1_transfer: ActorMethod<[TransferArg], TransferResult>;
+  icrc2_allowance: ActorMethod<[AllowanceArgs], Allowance>;
+  icrc2_approve: ActorMethod<[ApproveArgs], ApproveResult>;
+  icrc2_transfer_from: ActorMethod<[TransferFromArgs], TransferFromResult>;
 }
