8ebd81875e0c6dc21983c2fb64326b76246228b7
David Dal Busco <david.dalbusco@dfinity.org>
2023-09-26 14:26:34 +0200

    feat: move ICP

--- packages/ledger-icp/src/index.ts
+++ packages/ledger-icp/src/index.ts
@@ -1,4 +1,5 @@
 export { AccountIdentifier, SubAccount } from "./account_identifier";
 export * from "./errors/ledger.errors";
+export { ICP } from "./icp";
 export { LedgerCanister } from "./ledger.canister";
 export * from "./types/ledger.options";
