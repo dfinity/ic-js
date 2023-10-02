e89b30b6077a43c029a72e20f1fb4c5d389807d2
David Dal Busco <david.dalbusco@dfinity.org>
2023-10-02 12:45:09 +0200

    feat: don't copy deprecated ICP

--- packages/ledger-icp/src/index.ts
+++ packages/ledger-icp/src/index.ts
@@ -1,6 +1,5 @@
 export { AccountIdentifier, SubAccount } from "./account_identifier";
 export * from "./errors/ledger.errors";
-export { ICP } from "./icp";
 export { LedgerCanister } from "./ledger.canister";
 export * from "./types/ledger.options";
 export * from "./utils/account_identifier.utils";
