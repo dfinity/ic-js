481fb79bb5c4a17333eefb283a8765227a6d4713
David Dal Busco <david.dalbusco@dfinity.org>
2023-10-02 10:38:49 +0200

    feat: copy account related utils

--- packages/ledger-icp/src/index.ts
+++ packages/ledger-icp/src/index.ts
@@ -3,3 +3,5 @@ export * from "./errors/ledger.errors";
 export { ICP } from "./icp";
 export { LedgerCanister } from "./ledger.canister";
 export * from "./types/ledger.options";
+export * from "./utils/account_identifier.utils";
+export * from "./utils/accounts.utils";
---
+++ packages/ledger-icp/src/utils/accounts.utils.ts
@@ -0,0 +1,27 @@
+import { bigEndianCrc32 } from "@dfinity/utils";
+import { InvalidAccountIDError } from "../errors/governance.errors";
+
+/**
+ * Checks account id check sum
+ *
+ * @throws InvalidAccountIDError
+ */
+export const checkAccountId = (accountId: string): void => {
+  // Verify the checksum of the given address.
+  if (accountId.length != 64) {
+    throw new InvalidAccountIDError(
+      `Invalid account identifier ${accountId}. The account identifier must be 64 chars in length.`,
+    );
+  }
+
+  const toAccountBytes = Buffer.from(accountId, "hex");
+  const foundChecksum = toAccountBytes.slice(0, 4);
+  const expectedCheckum = Buffer.from(bigEndianCrc32(toAccountBytes.slice(4)));
+  if (!expectedCheckum.equals(foundChecksum)) {
+    throw new InvalidAccountIDError(
+      `Account identifier ${accountId} has an invalid checksum. Are you sure the account identifier is correct?\n\nExpected checksum: ${expectedCheckum.toString(
+        "hex",
+      )}\nFound checksum: ${foundChecksum.toString("hex")}`,
+    );
+  }
+};
