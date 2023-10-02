a8df56d38fef2a256ebc9406f7f92f0263ddb17f
David Dal Busco <david.dalbusco@dfinity.org>
2023-10-02 08:11:36 +0200

    build: publish to npm only if needed

---
+++ scripts/publish-npm.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Reference: NPM RRFC --if-needed https://github.com/npm/rfcs/issues/466
+
+function publish_npm() {
+  local lib=$1
+
+  LOCAL_SHASUM=$(npm show @dfinity/"$lib" dist.shasum)
+  NPM_TARBALL=$(npm show @dfinity/"$lib" dist.tarball)
+  NPM_SHASUM=$(curl -s "$NPM_TARBALL" 2>&1 | shasum  | cut -f1 -d' ')
+
+  if [ "$LOCAL_SHASUM" == "$NPM_SHASUM" ]; then
+    echo "No changes in @dfinity/$lib need to be published to NPM."
+  else
+    npm publish --workspace=packages/"$lib" --provenance
+  fi
+}
+
+# Tips: libs use by other libs first
+LIBS=utils,ledger,nns-proto,nns,sns,cmc,ckbtc,ic-management
+
+for lib in $(echo $LIBS | sed "s/,/ /g")
+do
+    publish_npm "$lib"
+done
