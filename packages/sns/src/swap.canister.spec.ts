d2fc60f63b78657f771466ea4d029b1d9e5380c0
Llorenç <llorenc.muntaner@dfinity.org>
2023-10-11 09:16:55 +0200

    GIX-1934: Fix test

--- packages/sns/src/swap.canister.spec.ts
+++ packages/sns/src/swap.canister.spec.ts
@@ -210,6 +210,8 @@ describe("Swap canister", () => {
           sale_delay_seconds: [],
           max_participant_icp_e8s: BigInt(500_000_000),
           min_icp_e8s: BigInt(200_000_000),
+          min_direct_participation_icp_e8s: [1_000_000_000n],
+          max_direct_participation_icp_e8s: [2_000_000_000n],
         },
       ],
     };
