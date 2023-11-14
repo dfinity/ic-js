8a6f5d9761b63a2b1acbfbd42b22b675a58405d3
David Dal Busco <david.dalbusco@dfinity.org>
2023-11-14 14:02:00 +0100

    feat: ICP index canister getTransactions (#466)
    # Motivation
    
    Add support to fetch transactions with the ICP Index canister.
    
    # Changes
    
    - introduce `get_account_identifier_transactions`

--- packages/ledger-icp/src/index.canister.ts
+++ packages/ledger-icp/src/index.canister.ts
@@ -1,8 +1,18 @@
-import { Canister, createServices, type CanisterOptions } from "@dfinity/utils";
-import type { _SERVICE as IndexService } from "../candid/index";
+import {
+  Canister,
+  createServices,
+  toNullable,
+  type CanisterOptions,
+} from "@dfinity/utils";
+import type {
+  GetAccountIdentifierTransactionsResponse,
+  _SERVICE as IndexService,
+} from "../candid/index";
 import { idlFactory as certifiedIdlFactory } from "../candid/ledger.certified.idl";
 import { idlFactory } from "../candid/ledger.idl";
 import { MAINNET_INDEX_CANISTER_ID } from "./constants/canister_ids";
+import { IndexError } from "./errors/index.errors";
+import type { GetTransactionsParams } from "./types/index.params";
 import type { AccountBalanceParams } from "./types/ledger.params";
 import { paramToAccountIdentifier } from "./utils/params.utils";
 
@@ -39,4 +49,36 @@ export class IndexCanister extends Canister<IndexService> {
     this.caller({ certified }).get_account_identifier_balance(
       paramToAccountIdentifier(accountIdentifier).toHex(),
     );
+
+  /**
+   * Returns the transactions and balance of an ICP account.
+   *
+   * @param {GetTransactionsParams} params The parameters to get the transactions.
+   * @param {boolean} params.certified query or update call.
+   * @param {AccountIdentifierParam} params.accountIdentifier The account identifier provided either as hex string or as an AccountIdentifier.
+   * @param {bigint} params.start If set then the results will start from the next most recent transaction id after start (start won't be included). If not provided, then the results will start from the most recent transaction id.
+   * @param {bigint} params.maxResults Maximum number of transactions to fetch.
+   * @returns {Promise<GetAccountIdentifierTransactionsResponse>} The transactions, balance and the transaction id of the oldest transaction the account has.
+   * @throws {@link IndexError}
+   */
+  getTransactions = async ({
+    certified,
+    accountIdentifier,
+    start,
+    maxResults: max_results,
+  }: GetTransactionsParams): Promise<GetAccountIdentifierTransactionsResponse> => {
+    const response = await this.caller({
+      certified,
+    }).get_account_identifier_transactions({
+      account_identifier: paramToAccountIdentifier(accountIdentifier).toHex(),
+      start: toNullable(start),
+      max_results,
+    });
+
+    if ("Err" in response) {
+      throw new IndexError(response.Err.message);
+    }
+
+    return response.Ok;
+  };
 }
