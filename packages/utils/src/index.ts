809ad1885aec798523f88a921e4f25a795cd5e67
David Dal Busco <david.dalbusco@dfinity.org>
2023-11-14 07:52:39 +0100

    feat: seconds to duration

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -10,6 +10,7 @@ export * from "./utils/arrays.utils";
 export * from "./utils/asserts.utils";
 export * from "./utils/base32.utils";
 export * from "./utils/crc.utils";
+export * from "./utils/date.utils";
 export * from "./utils/debounce.utils";
 export * from "./utils/did.utils";
 export * from "./utils/json.utils";
