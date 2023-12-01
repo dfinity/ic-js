57cef857983851a6ce3e380bba934dc3b11eed6b
David de Kloet <david.de.kloet@dfinity.org>
2023-12-01 11:25:16 +0100

    Duplicate for easy diffing

--- packages/utils/src/parser/token.spec.ts
+++ packages/utils/src/parser/token.spec.ts
@@ -129,3 +129,131 @@ describe("ICP", () => {
     );
   });
 });
+
+describe("ICP", () => {
+  it("can be initialized from a whole number string", () => {
+    expect(TokenAmount.fromString({ token: ICPToken, amount: "1" })).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(100000000) }),
+    );
+    expect(TokenAmount.fromString({ token: ICPToken, amount: "1234" })).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(123400000000) }),
+    );
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "000001234" }),
+    ).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(123400000000) }),
+    );
+    expect(TokenAmount.fromString({ token: ICPToken, amount: " 1" })).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(100000000) }),
+    );
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "1,000" }),
+    ).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(100000000000) }),
+    );
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "1'000" }),
+    ).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(100000000000) }),
+    );
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "1'000'000" }),
+    ).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(100000000000000) }),
+    );
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "1'000'000" }),
+    ).toEqual(TokenAmount.fromNumber({ token: ICPToken, amount: 1_000_000 }));
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "1'000'000" }),
+    ).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(100000000000000) }),
+    );
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "1'000" }),
+    ).toEqual(TokenAmount.fromNumber({ token: ICPToken, amount: 1_000 }));
+  });
+
+  it("can be initialized from a fractional number string", () => {
+    expect(TokenAmount.fromString({ token: ICPToken, amount: "0.1" })).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(10000000) }),
+    );
+    expect(TokenAmount.fromString({ token: ICPToken, amount: "0.1" })).toEqual(
+      TokenAmount.fromNumber({ token: ICPToken, amount: 0.1 }),
+    );
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "45.1231" }),
+    ).toEqual(TokenAmount.fromNumber({ token: ICPToken, amount: 45.1231 }));
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "0.3319" }),
+    ).toEqual(TokenAmount.fromNumber({ token: ICPToken, amount: 0.3319 }));
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "0.0001" }),
+    ).toEqual(TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(10000) }));
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "0.00000001" }),
+    ).toEqual(TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(1) }));
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "0.0000000001" }),
+    ).toEqual(FromStringToTokenError.FractionalMoreThan8Decimals);
+    expect(TokenAmount.fromString({ token: ICPToken, amount: ".01" })).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(1000000) }),
+    );
+  });
+
+  it("can be initialized from a mixed string", () => {
+    expect(TokenAmount.fromString({ token: ICPToken, amount: "1.1" })).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(110000000) }),
+    );
+    expect(TokenAmount.fromString({ token: ICPToken, amount: "1.1" })).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(110000000) }),
+    );
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "12,345.00000001" }),
+    ).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(1234500000001) }),
+    );
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "12'345.00000001" }),
+    ).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(1234500000001) }),
+    );
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "12345.00000001" }),
+    ).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(1234500000001) }),
+    );
+    expect(TokenAmount.fromString({ token: ICPToken, amount: "1e-8" })).toEqual(
+      TokenAmount.fromE8s({ token: ICPToken, amount: BigInt(1) }),
+    );
+  });
+
+  it("returns an error on invalid formats", () => {
+    expect(TokenAmount.fromString({ token: ICPToken, amount: "1.1.1" })).toBe(
+      FromStringToTokenError.InvalidFormat,
+    );
+    expect(TokenAmount.fromString({ token: ICPToken, amount: "a" })).toBe(
+      FromStringToTokenError.InvalidFormat,
+    );
+    expect(TokenAmount.fromString({ token: ICPToken, amount: "3.a" })).toBe(
+      FromStringToTokenError.InvalidFormat,
+    );
+    expect(
+      TokenAmount.fromString({ token: ICPToken, amount: "123asdf$#@~!" }),
+    ).toBe(FromStringToTokenError.InvalidFormat);
+
+    const callToNumber = () =>
+      TokenAmount.fromNumber({ token: ICPToken, amount: 1e-9 });
+    expect(callToNumber).toThrow(
+      expect.objectContaining({
+        message: "Number 1e-9 has more than 8 decimals",
+      }),
+    );
+  });
+
+  it("rejects negative numbers", () => {
+    expect(TokenAmount.fromString({ token: ICPToken, amount: "-1" })).toBe(
+      FromStringToTokenError.InvalidFormat,
+    );
+  });
+});
--- packages/utils/src/parser/token.ts
+++ packages/utils/src/parser/token.ts
@@ -54,6 +54,59 @@ export const convertStringToE8s = (
   return e8s;
 };
 
+/**
+ * Receives a string representing a number and returns the big int or error.
+ *
+ * @param amount - in string format
+ * @returns bigint | FromStringToTokenError
+ */
+export const convertStringToE8s = (
+  value: string,
+): bigint | FromStringToTokenError => {
+  // replace exponential format (1e-4) with plain (0.0001)
+  // doesn't support decimals for values >= ~1e16
+  let amount = value.includes("e")
+    ? Number(value).toLocaleString("en", {
+        useGrouping: false,
+        maximumFractionDigits: 20,
+      })
+    : value;
+
+  // Remove all instances of "," and "'".
+  amount = amount.trim().replace(/[,']/g, "");
+
+  // Verify that the string is of the format 1234.5678
+  const regexMatch = amount.match(/\d*(\.\d*)?/);
+  if (!regexMatch || regexMatch[0] !== amount) {
+    return FromStringToTokenError.InvalidFormat;
+  }
+
+  const [integral, fractional] = amount.split(".");
+
+  let e8s = BigInt(0);
+
+  if (integral) {
+    try {
+      e8s += BigInt(integral) * E8S_PER_TOKEN;
+    } catch {
+      return FromStringToTokenError.InvalidFormat;
+    }
+  }
+
+  if (fractional) {
+    if (fractional.length > 8) {
+      return FromStringToTokenError.FractionalMoreThan8Decimals;
+    }
+    try {
+      e8s += BigInt(fractional.padEnd(8, "0"));
+    } catch {
+      return FromStringToTokenError.InvalidFormat;
+    }
+  }
+
+  return e8s;
+};
+
 export interface Token {
   symbol: string;
   name: string;
@@ -63,6 +116,101 @@ export const ICPToken: Token = {
   symbol: "ICP",
   name: "Internet Computer",
 };
+
+/**
+ * Represents an amount of tokens.
+ *
+ * @param e8s - The amount of tokens in bigint.
+ * @param token - The token type.
+ */
+export class TokenAmount {
+  private constructor(
+    protected e8s: bigint,
+    public token: Token,
+  ) {}
+
+  /**
+   * Initialize from a bigint. Bigint are considered e8s.
+   *
+   * @param {amount: bigint; token?: Token;} params
+   * @param {bigint} params.amount The amount in bigint format.
+   * @param {Token} params.token The token type.
+   */
+  public static fromE8s({
+    amount,
+    token,
+  }: {
+    amount: bigint;
+    token: Token;
+  }): TokenAmount {
+    return new TokenAmount(amount, token);
+  }
+
+  /**
+   * Initialize from a string. Accepted formats:
+   *
+   * 1234567.8901
+   * 1'234'567.8901
+   * 1,234,567.8901
+   *
+   * @param {amount: string; token?: Token;} params
+   * @param {string} params.amount The amount in string format.
+   * @param {Token} params.token The token type.
+   */
+  public static fromString({
+    amount,
+    token,
+  }: {
+    amount: string;
+    token: Token;
+  }): TokenAmount | FromStringToTokenError {
+    const e8s = convertStringToE8s(amount);
+
+    if (typeof e8s === "bigint") {
+      return new TokenAmount(e8s, token);
+    }
+    return e8s;
+  }
+
+  /**
+   * Initialize from a number.
+   *
+   * 1 integer is considered E8S_PER_TOKEN
+   *
+   * @param {amount: number; token?: Token;} params
+   * @param {string} params.amount The amount in number format.
+   * @param {Token} params.token The token type.
+   */
+  public static fromNumber({
+    amount,
+    token,
+  }: {
+    amount: number;
+    token: Token;
+  }): TokenAmount {
+    const tokenAmount = TokenAmount.fromString({
+      amount: amount.toString(),
+      token,
+    });
+    if (tokenAmount instanceof TokenAmount) {
+      return tokenAmount;
+    }
+    if (tokenAmount === FromStringToTokenError.FractionalMoreThan8Decimals) {
+      throw new Error(`Number ${amount} has more than 8 decimals`);
+    }
+
+    // This should never happen
+    throw new Error(`Invalid number ${amount}`);
+  }
+
+  /**
+   *
+   * @returns The amount of e8s.
+   */
+  public toE8s(): bigint {
+    return this.e8s;
+  }
+}
 
 /**
  * Represents an amount of tokens.
