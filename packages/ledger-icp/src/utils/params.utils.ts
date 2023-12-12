e5e42849a62aaf45f35cca516b43b4a0e24abe4a
David Dal Busco <david.dalbusco@dfinity.org>
2023-12-12 07:11:52 +0100

    feat: convert param only if needed

--- packages/ledger-icp/src/utils/params.utils.ts
+++ packages/ledger-icp/src/utils/params.utils.ts
@@ -1,7 +1,13 @@
 import { AccountIdentifier } from "../account_identifier";
+import type { AccountIdentifierHex } from "../types/common";
 import type { AccountIdentifierParam } from "../types/ledger.params";
 
 export const paramToAccountIdentifier = (
   param: AccountIdentifierParam,
 ): AccountIdentifier =>
   param instanceof AccountIdentifier ? param : AccountIdentifier.fromHex(param);
+
+export const paramToAccountIdentifierHex = (
+  param: AccountIdentifierParam,
+): AccountIdentifierHex =>
+  param instanceof AccountIdentifier ? param.toHex() : param;
