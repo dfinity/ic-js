3ef842b02a1673947c2d11c9f5b9a8f5aa2eda7c
David de Kloet <david.de.kloet@dfinity.org>
2023-12-14 09:29:56 +0100

    Add retrieveBtcStatusV2ByAccount to ckbtc canister

--- packages/ckbtc/src/minter.canister.ts
+++ packages/ckbtc/src/minter.canister.ts
@@ -1,6 +1,7 @@
 import {
   Canister,
   createServices,
+  fromNullable,
   toNullable,
   type QueryParams,
 } from "@dfinity/utils";
@@ -28,6 +29,7 @@ import type {
 import type {
   EstimateWithdrawalFee,
   RetrieveBtcResponse,
+  RetrieveBtcStatusV2WithId,
   RetrieveBtcWithApprovalResponse,
   UpdateBalanceOk,
   UpdateBalanceResponse,
@@ -187,6 +189,26 @@ export class CkBTCMinterCanister extends Canister<CkBTCMinterService> {
     this.caller({
       certified,
     }).retrieve_btc_status({ block_index: transactionId });
+
+  /**
+   * Returns the status of all BTC withdrawals for the user's main account.
+   *
+   * @param {bigint} transactionId The ID of the corresponding burn transaction.
+   * @param {boolean} certified query or update call
+   */
+  retrieveBtcStatusV2ByAccount = async ({
+    certified,
+  }: {
+    certified: boolean;
+  }): Promise<RetrieveBtcStatusV2WithId[]> => {
+    const statuses = await this.caller({
+      certified,
+    }).retrieve_btc_status_v2_by_account([]);
+    return statuses.map(({ block_index, status_v2 }) => ({
+      id: block_index,
+      status: fromNullable(status_v2),
+    }));
+  };
 
   /**
    * Returns an estimation of the user's fee (in Satoshi) for a retrieve_btc request based on the current status of the Bitcoin network and the fee related to the minter.
--- packages/ckbtc/src/types/minter.responses.ts
+++ packages/ckbtc/src/types/minter.responses.ts
@@ -1,6 +1,7 @@
 import type {
   RetrieveBtcError,
   RetrieveBtcOk,
+  RetrieveBtcStatusV2,
   RetrieveBtcWithApprovalError,
   UpdateBalanceError,
   UtxoStatus,
@@ -21,3 +22,8 @@ export type RetrieveBtcWithApprovalResponse =
   | { Err: RetrieveBtcWithApprovalError };
 
 export type EstimateWithdrawalFee = { minter_fee: bigint; bitcoin_fee: bigint };
+
+export type RetrieveBtcStatusV2WithId = {
+  id: bigint;
+  status: RetrieveBtcStatusV2 | undefined;
+};
