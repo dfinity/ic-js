325a5d39e7f0cbd3c64e572befa1a0620f67e4e6
Carly Gundy <47304080+cgundy@users.noreply.github.com>
2024-01-11 13:43:54 +0100

    test

--- packages/ckbtc/src/index.ts
+++ packages/ckbtc/src/index.ts
@@ -9,7 +9,8 @@ export type {
 export * from "./enums/btc.enums";
 export * from "./errors/btc.errors";
 export * from "./errors/minter.errors";
-export { CkBTCMinterCanister } from "./minter.canister";
+export { CkBTCMinterCanister 
+       } from "./minter.canister";
 export * from "./types/btc";
 export * from "./types/minter.params";
 export * from "./types/minter.responses";
