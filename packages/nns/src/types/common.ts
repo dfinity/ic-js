fe4e1fa822b1e309498dbb353f549c0c34af8515
David Dal Busco <david.dalbusco@dfinity.org>
2024-02-12 06:42:25 +0100

    feat: clean-up AccountIdentifier string

--- packages/nns/src/types/common.ts
+++ packages/nns/src/types/common.ts
@@ -1,6 +1,5 @@
 export type CanisterIdString = string;
 export type NeuronId = bigint;
-export type AccountIdentifier = string;
 export type E8s = bigint;
 export type Memo = bigint;
 export type PrincipalString = string;
