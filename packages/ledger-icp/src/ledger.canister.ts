f844f2401e796546ace93d1e5346e02634f8ac32
David de Kloet <122978264+dskloetd@users.noreply.github.com>
2024-03-27 16:11:39 +0100

    NNS1-2886: Remove hardware wallet specific code paths from ledger-icp (#595)
    # Motivation
    
    The NNS dapp now requires version 2.4.9 of the Internet Computer Ledger
    app.
    See for context:
    https://forum.dfinity.org/t/nns-dapp-to-remove-protobuf-dependency-upgrade-your-ledger-ic-app-to-2-4-9/27712
    This means that we no longer need to use a separate code path with
    protocol buffers when making hardware wallet transactions but can
    instead use the exact same code path as for normal transactions.
    
    NNS-dapp already doesn't use hardware wallet specific code paths since
    https://github.com/dfinity/nns-dapp/pull/4373
    
    # Changes
    
    1. Remove all code paths in `packages/ledger-icp` that check
    `this.hardwareWallet`.
    2. Remove all functions that became unused as a result.
    
    # Tests
    
    Remove hardware wallet specific unit tests.
    
    # Todos
    
    - [x] Add entry to changelog (if necessary).
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/ledger-icp/src/ledger.canister.ts
+++ packages/ledger-icp/src/ledger.canister.ts
@@ -4,19 +4,14 @@ import { createServices } from "@dfinity/utils";
 import type { _SERVICE as LedgerService } from "../candid/ledger";
 import { idlFactory as certifiedIdlFactory } from "../candid/ledger.certified.idl";
 import { idlFactory } from "../candid/ledger.idl";
-import type { AccountIdentifier } from "./account_identifier";
 import {
-  subAccountNumbersToSubaccount,
-  toICPTs,
   toIcrc1TransferRawRequest,
   toTransferRawRequest,
 } from "./canisters/ledger/ledger.request.converts";
 import { MAINNET_LEDGER_CANISTER_ID } from "./constants/canister_ids";
-import { TRANSACTION_FEE } from "./constants/constants";
 import {
   mapIcrc1TransferError,
   mapTransferError,
-  mapTransferProtoError,
 } from "./errors/ledger.errors";
 import type { BlockHeight } from "./types/common";
 import type {
@@ -29,7 +24,7 @@ import type {
   TransferRequest,
 } from "./types/ledger_converters";
 import { paramToAccountIdentifier } from "./utils/params.utils";
-import { importNnsProto, queryCall, updateCall } from "./utils/proto.utils";
+import { queryCall, updateCall } from "./utils/proto.utils";
 
 export class LedgerCanister {
   private constructor(
@@ -84,12 +79,6 @@ export class LedgerCanister {
   }: AccountBalanceParams): Promise<bigint> => {
     const accountIdentifier = paramToAccountIdentifier(accountIdentifierParam);
 
-    if (this.hardwareWallet) {
-      return this.accountBalanceHardwareWallet({
-        accountIdentifier,
-        certified,
-      });
-    }
     const service = certified ? this.certifiedService : this.service;
     const tokens = await service.account_balance({
       account: accountIdentifier.toUint8Array(),
@@ -115,9 +104,6 @@ export class LedgerCanister {
    * @throws {@link TransferError}
    */
   public transfer = async (request: TransferRequest): Promise<BlockHeight> => {
-    if (this.hardwareWallet) {
-      return this.transferHardwareWallet(request);
-    }
     const rawRequest = toTransferRawRequest(request);
     const response = await this.certifiedService.transfer(rawRequest);
     if ("Err" in response) {
@@ -146,88 +132,5 @@ export class LedgerCanister {
       throw mapIcrc1TransferError(response.Err);
     }
     return response.Ok;
-  };
-
-  private accountBalanceHardwareWallet = async ({
-    accountIdentifier,
-    certified = true,
-  }: {
-    accountIdentifier: AccountIdentifier;
-    certified?: boolean;
-  }): Promise<bigint> => {
-    const callMethod = certified ? this.updateFetcher : this.queryFetcher;
-
-    const { AccountBalanceRequest: AccountBalanceRequestConstructor, ICPTs } =
-      await importNnsProto();
-
-    const request = new AccountBalanceRequestConstructor();
-    request.setAccount(await accountIdentifier.toProto());
-
-    const responseBytes = await callMethod({
-      agent: this.agent,
-      canisterId: this.canisterId,
-      methodName: "account_balance_pb",
-      arg: request.serializeBinary(),
-    });
-
-    return BigInt(
-      ICPTs.deserializeBinary(new Uint8Array(responseBytes)).getE8s(),
-    );
-  };
-
-  private transferHardwareWallet = async ({
-    to,
-    amount,
-    memo,
-    fee,
-    fromSubAccount,
-    createdAt,
-  }: TransferRequest): Promise<BlockHeight> => {
-    const { SendRequest, Payment, Memo, TimeStamp, BlockHeight } =
-      await importNnsProto();
-
-    const request = new SendRequest();
-    request.setTo(await to.toProto());
-
-    const payment = new Payment();
-    payment.setReceiverGets(await toICPTs(amount));
-    request.setPayment(payment);
-
-    request.setMaxFee(await toICPTs(fee ?? TRANSACTION_FEE));
-
-    // Always explicitly set the memo for compatibility with ledger wallet - hardware wallet
-    const requestMemo = new Memo();
-    requestMemo.setMemo((memo ?? BigInt(0)).toString());
-    request.setMemo(requestMemo);
-
-    if (createdAt !== undefined) {
-      const timestamp = new TimeStamp();
-      timestamp.setTimestampNanos(createdAt.toString());
-      request.setCreatedAtTime(timestamp);
-    }
-
-    if (fromSubAccount !== undefined) {
-      request.setFromSubaccount(
-        await subAccountNumbersToSubaccount(fromSubAccount),
-      );
-    }
-
-    try {
-      const responseBytes = await this.updateFetcher({
-        agent: this.agent,
-        canisterId: this.canisterId,
-        methodName: "send_pb",
-        arg: request.serializeBinary(),
-      });
-
-      // Successful tx. Return the block height.
-      return BigInt(BlockHeight.deserializeBinary(responseBytes).getHeight());
-    } catch (err) {
-      if (err instanceof Error) {
-        throw mapTransferProtoError(err);
-      }
-
-      throw err;
-    }
   };
 }
