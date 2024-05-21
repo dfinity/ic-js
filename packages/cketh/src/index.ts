859846245d354210e479df63967d5885af2a2eee
David Dal Busco <david.dalbusco@dfinity.org>
2024-05-21 08:27:29 +0200

    feat: expose ckEth orchestrator
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- packages/cketh/src/index.ts
+++ packages/cketh/src/index.ts
@@ -6,6 +6,14 @@ export type {
   RetrieveEthStatus,
   TxFinalizedStatus,
 } from "../candid/minter";
+export type {
+  CyclesManagement,
+  Erc20Contract,
+  ManagedCanisterStatus,
+  ManagedCanisters,
+  OrchestratorInfo,
+} from "../candid/orchestrator";
 export * from "./errors/minter.errors";
 export { CkETHMinterCanister } from "./minter.canister";
+export { CkETHOrchestratorCanister } from "./orchestrator.canister";
 export * from "./utils/minter.utils";
