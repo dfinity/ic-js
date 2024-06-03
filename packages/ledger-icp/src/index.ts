7540dcdbfd88ab1973f148e4e7f0efb40dab4ef7
David Dal Busco <david.dalbusco@dfinity.org>
2024-06-03 10:26:36 +0200

    fix: expose icrc1 types in ICP lib
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- packages/ledger-icp/src/index.ts
+++ packages/ledger-icp/src/index.ts
@@ -1,6 +1,8 @@
 export type * from "../candid/index";
 export type {
+  Account as Icrc1Account,
   Icrc1BlockIndex,
+  SubAccount as Icrc1SubAccount,
   Icrc1Timestamp,
   Icrc1Tokens,
   Value,
@@ -11,5 +13,10 @@ export { IndexCanister } from "./index.canister";
 export { LedgerCanister } from "./ledger.canister";
 export type * from "./types/common";
 export * from "./types/ledger.options";
+export type {
+  Icrc1TransferRequest,
+  Icrc2ApproveRequest,
+  TransferRequest,
+} from "./types/ledger_converters";
 export * from "./utils/account_identifier.utils";
 export * from "./utils/accounts.utils";
