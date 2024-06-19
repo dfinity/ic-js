43bc7f0330aec3746b2e6b74817e92b15533140f
David de Kloet <david.de.kloet@dfinity.org>
2024-06-19 11:36:50 +0200

    Fix updateNeuron not to change the neuron subaccount

--- packages/nns/src/governance_test.canister.ts
+++ packages/nns/src/governance_test.canister.ts
@@ -1,17 +1,25 @@
 import type { ActorSubclass } from "@dfinity/agent";
+import { principalToAccountIdentifier } from "@dfinity/ledger-icp";
 import type { Principal } from "@dfinity/principal";
-import { createServices, type CanisterOptions } from "@dfinity/utils";
+import {
+  assertNonNullish,
+  createServices,
+  type CanisterOptions,
+} from "@dfinity/utils";
 import { idlFactory } from "../candid/governance.idl";
 import type { _SERVICE as GovernanceService } from "../candid/governance_test";
 import { idlFactory as certifiedIdlFactory } from "../candid/governance_test.certified.idl";
+import { fromListNeurons } from "./canisters/governance/request.converters";
 import { toRawNeuron } from "./canisters/governance/response.converters";
 import { MAINNET_GOVERNANCE_CANISTER_ID } from "./constants/canister_ids";
 import type { Neuron } from "./types/governance_converters";
 
 export class GovernanceTestCanister {
   private constructor(
+    private readonly canisterId: Principal,
     private readonly certifiedService: ActorSubclass<GovernanceService>,
   ) {
+    this.canisterId = canisterId;
     this.certifiedService = certifiedService;
   }
 
@@ -28,7 +36,7 @@ export class GovernanceTestCanister {
       certifiedIdlFactory,
     });
 
-    return new GovernanceTestCanister(certifiedService);
+    return new GovernanceTestCanister(canisterId, certifiedService);
   }
 
   /**
@@ -36,7 +44,24 @@ export class GovernanceTestCanister {
    *
    * Only available in the governance test canister.
    */
-  updateNeuron(neuron: Neuron) {
-    this.certifiedService.update_neuron(toRawNeuron(neuron));
+  async updateNeuron(neuron: Neuron) {
+    assertNonNullish(neuron.id);
+    const rawListNeuronsRequest = fromListNeurons([neuron.id]);
+    const rawListNeuronsResponse = await this.certifiedService.list_neurons(
+      rawListNeuronsRequest,
+    );
+    const currentNeuron = rawListNeuronsResponse.full_neurons[0];
+    const currentAccountIdentifier = principalToAccountIdentifier(
+      this.canisterId,
+      Uint8Array.from(currentNeuron.account),
+    );
+    if (currentAccountIdentifier !== neuron.accountIdentifier) {
+      throw new Error("Neuron account identifier can't be changed");
+    }
+    const rawNeuron = toRawNeuron({
+      neuron,
+      account: Uint8Array.from(currentNeuron.account),
+    });
+    return this.certifiedService.update_neuron(rawNeuron);
   }
 }
