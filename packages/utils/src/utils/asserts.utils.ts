5e7a64404d980d1112d959c209fa17192386debe
David de Kloet <david.de.kloet@dfinity.org>
2024-07-22 08:23:16 +0200

    const arrow

--- packages/utils/src/utils/asserts.utils.ts
+++ packages/utils/src/utils/asserts.utils.ts
@@ -1,19 +1,22 @@
 export class InvalidPercentageError extends Error {}
 export class NullishError extends Error {}
 
-export function assertNonNullish<T>(
+export const assertNonNullish: <T>(
   value: T,
   message?: string,
-): asserts value is NonNullable<T> {
+) => asserts value is NonNullable<T> = <T>(
+  value: T,
+  message?: string,
+): void => {
   if (value === null || value === undefined) {
     throw new NullishError(message);
   }
-}
+};
 
-export function asNonNullish<T>(value: T, message?: string): NonNullable<T> {
+export const asNonNullish = <T>(value: T, message?: string): NonNullable<T> => {
   assertNonNullish(value, message);
   return value;
-}
+};
 
 export const assertPercentageNumber = (percentage: number) => {
   if (percentage < 0 || percentage > 100) {
