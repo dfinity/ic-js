bfb13a9a4b915d9a55751046f93f72eef8a50eeb
Llorenç <llorenc.muntaner@dfinity.org>
2024-08-23 16:00:17 +0200

    Fix expiry

--- scripts/test-vectors/20240730-icrc21.ts
+++ scripts/test-vectors/20240730-icrc21.ts
@@ -1,6 +1,6 @@
-import { Expiry, SubmitRequestType } from "@dfinity/agent";
+import { Cbor } from "@dfinity/agent";
 import { Principal } from "@dfinity/principal";
-import { _prepareCborForLedger, writeToJson } from "./utils";
+import { writeToJson } from "./utils";
 
 function hexStringToArrayBuffer(hexString: string): ArrayBuffer {
   // Remove any non-hexadecimal characters (e.g., spaces)
@@ -26,9 +26,9 @@ function hexStringToArrayBuffer(hexString: string): ArrayBuffer {
 const callRequest = {
   arg: hexStringToArrayBuffer("4449444C00017104746F6269"),
   canister_id: Principal.fromHex("00000000006000FD0101"),
-  ingress_expiry: new Expiry(1712667140606 - Date.now()),
+  ingress_expiry: BigInt("1712667140606000000"),
   method_name: "greet",
-  request_type: "query" as SubmitRequestType,
+  request_type: "query",
   sender: Principal.fromHex("04"),
 };
 
@@ -37,19 +37,19 @@ const consentMessageRequest = {
     "4449444C076D7B6C01D880C6D007716C02CBAEB581017AB183E7F1077A6B028BEABFC2067F8EF1C1EE0D026E036C02EFCEE7800401C4FBF2DB05046C03D6FCA70200E1EDEB4A7184F7FEE80A0501060C4449444C00017104746F626905677265657402656E01011E000300",
   ),
   canister_id: Principal.fromHex("00000000006000FD0101"),
-  ingress_expiry: new Expiry(1712666698482 - Date.now()),
+  ingress_expiry: BigInt("1712666698482000000"),
   method_name: "icrc21_canister_call_consent_message",
   nonce: hexStringToArrayBuffer("A3788C1805553FB69B20F08E87E23B13"),
-  request_type: "call" as SubmitRequestType,
+  request_type: "call",
   sender: Principal.fromHex("04"),
 };
 
 const createCandidBlobs = () => {
   const callRequestBlob = Buffer.from(
-    _prepareCborForLedger(callRequest),
+    Cbor.encode({ content: callRequest }),
   ).toString("hex");
   const consentMessageRequestBlob = Buffer.from(
-    _prepareCborForLedger(consentMessageRequest),
+    Cbor.encode({ content: consentMessageRequest }),
   ).toString("hex");
 
   writeToJson({
