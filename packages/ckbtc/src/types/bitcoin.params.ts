79675d0cc8a0896ca3cbd850a59529360e713dcf
David Dal Busco <david.dalbusco@dfinity.org>
2024-08-27 10:01:58 +0200

    feat: ckBTC min_confirmations renamed to minConfirmations (#703)
    # Motivation
    
    As discussed in #701, let's rename `min_confirmations` parameter of
    `getUtxos` in ckBTC library to `minConfirmations`.
    
    # Changes
    
    - Parameter and mapping updated accordingly.
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- packages/ckbtc/src/types/bitcoin.params.ts
+++ packages/ckbtc/src/types/bitcoin.params.ts
@@ -1,4 +1,4 @@
-import { toNullable, type QueryParams } from "@dfinity/utils";
+import { nonNullish, toNullable, type QueryParams } from "@dfinity/utils";
 import type {
   get_balance_request,
   get_utxos_request,
@@ -12,7 +12,7 @@ const mapBitcoinNetwork = (network: BitcoinNetwork): network =>
 
 export type GetUtxosParams = Omit<get_utxos_request, "network" | "filter"> & {
   network: BitcoinNetwork;
-  filter?: { page: Uint8Array | number[] } | { min_confirmations: number };
+  filter?: { page: Uint8Array | number[] } | { minConfirmations: number };
 } & QueryParams;
 
 export const toGetUtxosParams = ({
@@ -20,8 +20,14 @@ export const toGetUtxosParams = ({
   filter,
   ...rest
 }: GetUtxosParams): get_utxos_request => ({
-  filter: toNullable(filter),
-  network: mapBitcoinNetwork(network),
+  filter: nonNullish(filter)
+    ? toNullable(
+        "minConfirmations" in filter
+          ? { min_confirmations: filter.minConfirmations }
+          : { page: filter.page },
+      )
+    : toNullable(),
+  network: network === "testnet" ? { testnet: null } : { mainnet: null },
   ...rest,
 });
 
