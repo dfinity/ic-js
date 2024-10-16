102799618cba2800d701de425f377adc28374bff
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-16 09:14:23 +0200

    chore: adjust docstring

--- packages/utils/src/utils/agent.utils.ts
+++ packages/utils/src/utils/agent.utils.ts
@@ -15,11 +15,13 @@ export const defaultAgent = (): Agent =>
 
 /**
  * Create an agent for a given identity
- * @param identity A mandatory identity to use for the agent
- * @param host An optional host to connect to
- * @param fetchRootKey Fetch root key for certificate validation during local development or on testnet
- * @param verifyQuerySignatures Check for signatures in the state tree signed by the node that replies to queries - i.e. certify responses.
- * @param retryTimes Set the number of retries the agent should perform before errorring.
+ *
+ * @param {CreateAgentParams} params The parameters to create a new HTTP agent
+ * @param {Identity} params.identity A mandatory identity to use for the agent
+ * @param {string} params.host An optional host to connect to
+ * @param {boolean} params.fetchRootKey Fetch root key for certificate validation during local development or on testnet
+ * @param {boolean} params.verifyQuerySignatures Check for signatures in the state tree signed by the node that replies to queries - i.e. certify responses.
+ * @param {number} params.retryTimes Set the number of retries the agent should perform before errorring.
  */
 export const createAgent = async ({
   identity,
