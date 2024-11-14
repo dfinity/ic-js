dccc080d29efcfbfaf6046d34d8a14cc2c31e341
Llorenç <llorenc.muntaner@dfinity.org>
2024-11-14 11:01:41 +0100

    Test vectors for set neuron visibility issue #249

---
+++ scripts/test-vectors/20241113-set-visibility.ts
@@ -0,0 +1,86 @@
+import { IDL } from "@dfinity/candid";
+import { NeuronId } from "@dfinity/nns/src";
+import { toSetVisibilityRequest } from "@dfinity/nns/src/canisters/governance/request.converters";
+import { NeuronVisibility } from "@dfinity/nns/src/enums/governance.enums";
+import { ManageNeuronFn } from "./governance.idl";
+import { createBlob, writeToJson } from "./utils";
+
+const mockNeuronId = BigInt(15374508381553347371);
+const mockNeuronId2 = BigInt(8836564053576662908);
+
+const createSetVisibilityVector = ({
+  neuronId,
+  visibility,
+}: {
+  neuronId: NeuronId;
+  visibility: NeuronVisibility;
+}) => {
+  const rawRequestBody = toSetVisibilityRequest({
+    neuronId,
+    visibility,
+  });
+
+  const messages = {
+    [NeuronVisibility.Private]: "Make Neuron Private",
+    [NeuronVisibility.Public]: "Make Neuron Public",
+    [NeuronVisibility.Unspecified]: "Unspecified",
+  };
+
+  const outputs = [
+    `Transaction type : ${messages[visibility]}`,
+    `Neuron ID : ${neuronId}`,
+  ];
+
+  return {
+    blob_candid: createBlob({
+      arg: IDL.encode(ManageNeuronFn.argTypes, [rawRequestBody]),
+      methodName: "manage_neuron",
+    }),
+    name: "Set Neuron Visibility",
+    candid_request: rawRequestBody,
+    outputs:
+      visibility === NeuronVisibility.Unspecified
+        ? [
+            `Fail: "${NeuronVisibility.Unspecified} is not a valid visibility option."`,
+          ]
+        : outputs,
+  };
+};
+
+const main = () => {
+  try {
+    const vectors = [
+      createSetVisibilityVector({
+        neuronId: mockNeuronId,
+        visibility: NeuronVisibility.Public,
+      }),
+      createSetVisibilityVector({
+        neuronId: mockNeuronId,
+        visibility: NeuronVisibility.Private,
+      }),
+      createSetVisibilityVector({
+        neuronId: mockNeuronId2,
+        visibility: NeuronVisibility.Public,
+      }),
+      createSetVisibilityVector({
+        neuronId: mockNeuronId,
+        visibility: NeuronVisibility.Private,
+      }),
+      createSetVisibilityVector({
+        neuronId: mockNeuronId,
+        visibility: NeuronVisibility.Unspecified,
+      }),
+    ];
+
+    writeToJson({
+      data: vectors,
+      fileName: "set-neuron-visibility.json",
+    });
+    console.log("File created successfully");
+  } catch (error) {
+    console.log("There was an error");
+    console.log(error);
+  }
+};
+
+main();
