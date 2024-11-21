a22db87989103b4d28018a90a3a31f096debe928
David Dal Busco <david.dalbusco@dfinity.org>
2024-11-21 12:29:01 +0100

    feat: expose ic-mgmt types (#762)
    # Motivation
    
    Few DID types should probably be exposed as they are part of the
    response but defined by Candid.
    
    # Changes
    
    - Expose few types in `index.ts` of ic-mgmt
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- packages/ic-management/src/index.ts
+++ packages/ic-management/src/index.ts
@@ -1,6 +1,10 @@
 export type {
   canister_log_record,
+  canister_status_result,
+  chunk_hash,
+  definite_canister_settings,
   fetch_canister_logs_result,
+  log_visibility,
   snapshot_id,
 } from "../candid/ic-management";
 export { ICManagementCanister } from "./ic-management.canister";
