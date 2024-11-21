f5b8426d6157b09bdde6ab141c2027ef4321fb3b
David Dal Busco <david.dalbusco@dfinity.org>
2024-11-21 07:05:38 +0100

    feat: expose ic-mgmt types
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- packages/ic-management/src/index.ts
+++ packages/ic-management/src/index.ts
@@ -1,6 +1,9 @@
 export type {
   canister_log_record,
+  canister_status_result,
+  definite_canister_settings,
   fetch_canister_logs_result,
+  log_visibility,
   snapshot_id,
 } from "../candid/ic-management";
 export { ICManagementCanister } from "./ic-management.canister";
