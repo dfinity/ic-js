cf27981204dcac114c0fe9b76c9341a6d87b0ab0
David Dal Busco <david.dalbusco@dfinity.org>
2024-12-16 10:26:59 +0100

    docs: format jsdocs for nullish.utils (#791)
    # Motivation
    
    I noticed the jsdocs in `nullish.utils` was not yet well formatted.
    
    # Changes
    
    - Format jsdocs with `@template`, `@param` etc.
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/utils/src/utils/nullish.utils.ts
+++ packages/utils/src/utils/nullish.utils.ts
@@ -1,13 +1,30 @@
-/** Is null or undefined */
+/**
+ * Checks if a given argument is null or undefined.
+ *
+ * @template T - The type of the argument.
+ * @param {T | undefined | null} argument - The argument to check.
+ * @returns {argument is undefined | null} `true` if the argument is null or undefined; otherwise, `false`.
+ */
 export const isNullish = <T>(
   argument: T | undefined | null,
 ): argument is undefined | null => argument === null || argument === undefined;
 
-/** Not null and not undefined */
+/**
+ * Checks if a given argument is neither null nor undefined.
+ *
+ * @template T - The type of the argument.
+ * @param {T | undefined | null} argument - The argument to check.
+ * @returns {argument is NonNullable<T>} `true` if the argument is not null or undefined; otherwise, `false`.
+ */
 export const nonNullish = <T>(
   argument: T | undefined | null,
 ): argument is NonNullable<T> => !isNullish(argument);
 
-/** Not null and not undefined and not empty */
+/**
+ * Checks if a given value is not null, not undefined, and not an empty string.
+ *
+ * @param {string | undefined | null} value - The value to check.
+ * @returns {boolean} `true` if the value is not null, not undefined, and not an empty string; otherwise, `false`.
+ */
 export const notEmptyString = (value: string | undefined | null): boolean =>
   nonNullish(value) && value !== "";
