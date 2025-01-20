96b062891636f883d834b427ba322dae19ed7105
David Dal Busco <david.dalbusco@dfinity.org>
2025-01-20 07:03:34 +0100

    feat: jsdocs
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- packages/zod-schemas/src/principal.ts
+++ packages/zod-schemas/src/principal.ts
@@ -1,6 +1,18 @@
 import {Principal} from '@dfinity/principal';
 import * as z from 'zod';
 
+/**
+ * Zod schema to validate a string as a valid textual representation of a Principal.
+ *
+ * This schema checks if the provided string can be converted into a `Principal` instance.
+ * If the conversion fails, validation will return an error message.
+ *
+ * @example
+ * ```typescript
+ * const result = PrincipalTextSchema.safeParse('aaaaa-aa');
+ * console.log(result.success); // true or false
+ * ```
+ */
 export const PrincipalTextSchema = z.string().refine(
   (principal) => {
     try {
