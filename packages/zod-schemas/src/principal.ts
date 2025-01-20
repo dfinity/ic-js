f18628c6c16246979511177dc942e43940227250
David Dal Busco <david.dalbusco@dfinity.org>
2025-01-20 06:58:05 +0100

    feat: zod schema for principal as text
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

---
+++ packages/zod-schemas/src/principal.ts
@@ -0,0 +1,18 @@
+import {Principal} from '@dfinity/principal';
+import * as z from 'zod';
+
+export const PrincipalTextSchema = z.string().refine(
+  (principal) => {
+    try {
+      Principal.fromText(principal);
+      return true;
+    } catch (err: unknown) {
+      return false;
+    }
+  },
+  {
+    message: 'Invalid textual representation of a Principal.'
+  }
+);
+
+export type PrincipalText = z.infer<typeof PrincipalTextSchema>;
