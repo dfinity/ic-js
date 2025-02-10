39193ca1ecd85130b85de297861284a059986538
David Dal Busco <david.dalbusco@dfinity.org>
2025-02-10 16:19:55 +0100

    docs: review Icrc2ApproveRequest jsdocs types
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- packages/ledger-icp/src/types/ledger_converters.ts
+++ packages/ledger-icp/src/types/ledger_converters.ts
@@ -43,12 +43,12 @@ export type Icrc1TransferRequest = {
  *
  * @param {Account} spender The account of the spender.
  * @param {Tokens} amount The amount of tokens to approve.
- * @param {Subaccount?} from_subaccount The subaccount to transfer tokens from.
- * @param {Uint8Array|number?} icrc1Memo Approve memo.
- * @param {Timestamp?} created_at_time nanoseconds since unix epoc to trigger deduplication and avoid other issues
- * @param {Tokens?} fee The fee of the transfer when it's not the default fee.
- * @param {Tokens?} expected_allowance The optional allowance expected. If the expected_allowance field is set, the ledger MUST ensure that the current allowance for the spender from the caller's account is equal to the given value and return the AllowanceChanged error otherwise.
- * @param {Timestamp?} expires_at When the approval expires. If the field is set, it's greater than the current ledger time.
+ * @param {SubAccount?} from_subaccount The subaccount to transfer tokens from.
+ * @param {Uint8Array?} icrc1Memo Approve memo.
+ * @param {Icrc1Timestamp?} createdAt nanoseconds since unix epoc to trigger deduplication and avoid other issues
+ * @param {Icrc1Tokens?} fee The fee of the transfer when it's not the default fee.
+ * @param {Icrc1Tokens?} expected_allowance The optional allowance expected. If the expected_allowance field is set, the ledger MUST ensure that the current allowance for the spender from the caller's account is equal to the given value and return the AllowanceChanged error otherwise.
+ * @param {Icrc1Timestamp?} expires_at When the approval expires. If the field is set, it's greater than the current ledger time.
  */
 export type Icrc2ApproveRequest = Omit<Icrc1TransferRequest, "to"> & {
   expected_allowance?: Icrc1Tokens;
