dfinity/ic-js

commit 6ab5ec65f2b18cfa92052c7ca54ab716d2176414

feat(governance): implement pagination for list_neurons endpoint (#833)
# Motivation

As presented
[here](https://forum.dfinity.org/t/request-for-feedback-on-governance-list-neurons-pagination-api/40362)
and changed [here](https://github.com/dfinity/ic/pull/3358), the NNS
governance canister will implement pagination for the `list_neurons`
endpoint.

Pagination will only activate if the response contains more than 500
neurons. Currently, most of our cases involve requesting non-empty
neurons, and we do not have users with such a high number of neurons, so
for those users nothing has changed. However, the nns-dapp has a use
case, Reporting, where we also request empty neurons. In this case, we
have a few hundred users with more than 500 neurons.

Some
[insights](https://dfinity.slack.com/archives/C01R76CSJ4R/p1738328033654549?thread_ts=1738243719.968339&cid=C01R76CSJ4R)
from the Boundary Node.

Expect usage patterns:  
-  Non-empty neurons:  
   - All users with fewer than 500 neurons → 1 page  
-  Non-empty and empty neurons, such as Reporting feature:  
   - Most users with fewer than 500 neurons → 1 page  
   - Some users with approximately 5,000 neurons → 10 pages  
   - Few users with approximately 40,000 neurons → 80 pages  

# Changes

- Implements pagination by checking the number of available pages from
the initial call to `list-neurons`.

# Tests

- Added unit tests to check pagination.
- Manually tested locally in the nns-dapp with over 1000 neurons.


https://github.com/user-attachments/assets/c3a484aa-60a6-4b58-af92-23db109fb8d0

# Todos

- [ ] Add entry to changelog (if necessary).

---------

Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
Co-authored-by: Carly Gundy <47304080+cgundy@users.noreply.github.com>

packages/nns/src/governance.canister.ts
@@ -143,6 +143,9 @@ export class GovernanceCanister {
    * it is fetched using a query call.
    *
    * The backend treats `includeEmptyNeurons` as false if absent.
+   *
+   * The response from the canister might be paginated. In this case, all pages will be fetched in parallel and
+   * combined into a single return value.
    */
   public listNeurons = async ({
     certified = true,
@@ -157,30 +160,112 @@ export class GovernanceCanister {
     includePublicNeurons?: boolean;
     neuronSubaccounts?: NeuronSubaccount[];
   }): Promise<NeuronInfo[]> => {
+    const useOldMethod =
+      isNullish(includeEmptyNeurons) &&
+      isNullish(includePublicNeurons) &&
+      certified;
+
+    if (useOldMethod) {
+      return this.fetchNeuronsWithOldMethodAndNoPagination({
+        neuronIds,
+      });
+    }
+
+    // Fetch the first page to get the total number of pages
+    const firstPageResult = await this.fetchNeuronsPage({
+      certified,
+      neuronIds,
+      includeEmptyNeurons,
+      includePublicNeurons,
+      neuronSubaccounts,
+      pageNumber: 1n,
+    });
+    const { neurons: firstPageNeurons, totalPages } = firstPageResult;
+
+    // https://github.com/dfinity/ic/blob/de17b0d718d6f279e9da8cd0f1b5de17036a6102/rs/nns/governance/api/src/ic_nns_governance.pb.v1.rs#L3543
+    if (totalPages < 2n) return firstPageNeurons;
+
+    const pagePromises = [];
+    for (let pageNumber = 1n; pageNumber < totalPages; pageNumber++) {
+      pagePromises.push(
+        this.fetchNeuronsPage({
+          certified,
+          neuronIds,
+          includeEmptyNeurons,
+          includePublicNeurons,
+          pageNumber,
+          neuronSubaccounts,
+        }),
+      );
+    }
+
+    const pageResults = [firstPageResult, ...(await Promise.all(pagePromises))];
+    const allNeurons = pageResults.flatMap(({ neurons }) => neurons);
+
+    return allNeurons;
+  };
+
+  // The Ledger app version 2.4.9 does not support
+  // include_empty_neurons_readable_by_caller nor include_public_neurons_in_full_neurons,
+  // even when the field is absent,
+  // so we use the old service (which does not have these fields) if possible,
+  // in case the call will be signed by the Ledger device. We only have a
+  // certified version of the old service.
+  private fetchNeuronsWithOldMethodAndNoPagination = async ({
+    neuronIds,
+  }: {
+    neuronIds?: NeuronId[];
+  }): Promise<NeuronInfo[]> => {
+    const rawRequest = fromListNeurons({
+      neuronIds,
+    });
+
+    const service = this.oldListNeuronsCertifiedService;
+    const rawResponse = await service.list_neurons(rawRequest);
+
+    return toArrayOfNeuronInfo({
+      response: rawResponse,
+      canisterId: this.canisterId,
+    });
+  };
+
+  private fetchNeuronsPage = async ({
+    certified,
+    neuronIds,
+    includeEmptyNeurons,
+    includePublicNeurons,
+    pageNumber,
+    neuronSubaccounts,
+  }: {
+    certified: boolean;
+    neuronIds?: NeuronId[];
+    includeEmptyNeurons?: boolean;
+    includePublicNeurons?: boolean;
+    pageNumber: bigint;
+    neuronSubaccounts?: NeuronSubaccount[];
+  }): Promise<{ neurons: NeuronInfo[]; totalPages: bigint }> => {
+    // https://github.com/dfinity/ic/blob/59c4b87a337f1bd52a076c0f3e99acf155b79803/rs/nns/governance/src/governance.rs#L223
+    const PAGE_SIZE = 500n;
+
     const rawRequest = fromListNeurons({
       neuronIds,
       includeEmptyNeurons,
       includePublicNeurons,
       neuronSubaccounts,
+      pageNumber,
+      pageSize: PAGE_SIZE,
     });
-    // The Ledger app version 2.4.9 does not support
-    // include_empty_neurons_readable_by_caller nor include_public_neurons_in_full_neurons,
-    // even when the field is absent,
-    // so we use the old service (which does not have these fields) if possible,
-    // in case the call will be signed by the Ledger device. We only have a
-    // certified version of the old service.
-    const useOldMethod =
-      isNullish(includeEmptyNeurons) &&
-      isNullish(includePublicNeurons) &&
-      certified;
-    const service = useOldMethod
-      ? this.oldListNeuronsCertifiedService
-      : this.getGovernanceService(certified);
-    const raw_response = await service.list_neurons(rawRequest);
-    return toArrayOfNeuronInfo({
-      response: raw_response,
+
+    const service = this.getGovernanceService(certified);
+    const rawResponse = await service.list_neurons(rawRequest);
+    const neurons = toArrayOfNeuronInfo({
+      response: rawResponse,
       canisterId: this.canisterId,
     });
+
+    const totalPages = fromNullable(rawResponse.total_pages_available) ?? 1n;
+
+    return { neurons, totalPages };
   };
 
   /**

packages/nns/src/governance.canister.spec.ts
@@ -428,33 +428,97 @@ describe("GovernanceCanister", () => {
   });
 
   describe("GovernanceCanister.listNeurons", () => {
-    it("list user neurons", async () => {
+    it("list user neurons no pagination", async () => {
       const service = mock<ActorSubclass<GovernanceService>>();
       const certifiedService = mock<ActorSubclass<GovernanceService>>();
       const oldService = mock<ActorSubclass<GovernanceService>>();
-      certifiedService.list_neurons.mockResolvedValue(mockListNeuronsResponse);
+
+      certifiedService.list_neurons.mockResolvedValue({
+        ...mockListNeuronsResponse,
+        total_pages_available: [1n],
+      });
 
       const governance = GovernanceCanister.create({
         certifiedServiceOverride: certifiedService,
         serviceOverride: service,
         oldListNeuronsServiceOverride: oldService,
       });
+
+      expect(certifiedService.list_neurons).toBeCalledTimes(0);
+
       const neurons = await governance.listNeurons({
         certified: true,
         includeEmptyNeurons: true,
         includePublicNeurons: true,
       });
+
+      expect(certifiedService.list_neurons).toBeCalledTimes(1);
       expect(certifiedService.list_neurons).toBeCalledWith({
         neuron_ids: new BigUint64Array(),
         include_neurons_readable_by_caller: true,
         include_empty_neurons_readable_by_caller: [true],
         include_public_neurons_in_full_neurons: [true],
-        page_number: [],
-        page_size: [],
         neuron_subaccounts: [],
+        page_number: [1n],
+        page_size: [500n],
       });
-      expect(certifiedService.list_neurons).toBeCalledTimes(1);
       expect(neurons.length).toBe(1);
+      expect(service.list_neurons).not.toBeCalled();
+      expect(oldService.list_neurons).not.toBeCalled();
+    });
+
+    it("list user neurons with pagination", async () => {
+      const service = mock<ActorSubclass<GovernanceService>>();
+      const certifiedService = mock<ActorSubclass<GovernanceService>>();
+      const oldService = mock<ActorSubclass<GovernanceService>>();
+
+      certifiedService.list_neurons.mockResolvedValueOnce({
+        ...mockListNeuronsResponse,
+        total_pages_available: [2n],
+      });
+      certifiedService.list_neurons.mockResolvedValueOnce({
+        ...mockListNeuronsResponse,
+        total_pages_available: [2n],
+      });
+
+      const governance = GovernanceCanister.create({
+        certifiedServiceOverride: certifiedService,
+        serviceOverride: service,
+        oldListNeuronsServiceOverride: oldService,
+      });
+
+      expect(certifiedService.list_neurons).toBeCalledTimes(0);
+
+      const neurons = await governance.listNeurons({
+        certified: true,
+        includeEmptyNeurons: true,
+        includePublicNeurons: true,
+      });
+
+      // Get first page
+      expect(certifiedService.list_neurons).toHaveBeenNthCalledWith(1, {
+        neuron_ids: new BigUint64Array(),
+        include_neurons_readable_by_caller: true,
+        include_empty_neurons_readable_by_caller: [true],
+        include_public_neurons_in_full_neurons: [true],
+        neuron_subaccounts: [],
+        page_number: [1n],
+        page_size: [500n],
+      });
+
+      // Second call fetching the second page
+      expect(certifiedService.list_neurons).toHaveBeenNthCalledWith(2, {
+        neuron_ids: new BigUint64Array(),
+        include_neurons_readable_by_caller: true,
+        include_empty_neurons_readable_by_caller: [true],
+        include_public_neurons_in_full_neurons: [true],
+        neuron_subaccounts: [],
+        page_number: [1n],
+        page_size: [500n],
+      });
+
+      expect(certifiedService.list_neurons).toBeCalledTimes(2);
+      expect(neurons.length).toBe(2); // One neuron per page
       expect(service.list_neurons).not.toBeCalled();
       expect(oldService.list_neurons).not.toBeCalled();
     });
@@ -478,9 +542,9 @@ describe("GovernanceCanister", () => {
         include_neurons_readable_by_caller: true,
         include_empty_neurons_readable_by_caller: [false],
         include_public_neurons_in_full_neurons: [],
-        page_number: [],
-        page_size: [],
         neuron_subaccounts: [],
+        page_number: [1n],
+        page_size: [500n],
       });
       expect(service.list_neurons).toBeCalledTimes(1);
       expect(neurons.length).toBe(1);
@@ -505,9 +569,9 @@ describe("GovernanceCanister", () => {
         include_neurons_readable_by_caller: true,
         include_empty_neurons_readable_by_caller: [],
         include_public_neurons_in_full_neurons: [false],
-        page_number: [],
-        page_size: [],
         neuron_subaccounts: [],
+        page_number: [1n],
+        page_size: [500n],
       });
       expect(service.list_neurons).toBeCalledTimes(1);
       expect(neurons.length).toBe(1);
@@ -537,8 +601,8 @@ describe("GovernanceCanister", () => {
         include_neurons_readable_by_caller: true,
         include_empty_neurons_readable_by_caller: [true],
         include_public_neurons_in_full_neurons: [true],
-        page_number: [],
-        page_size: [],
+        page_number: [1n],
+        page_size: [500n],
         neuron_subaccounts: [
           [
             { subaccount: new Uint8Array([1, 2, 3]) },
@@ -601,8 +665,8 @@ describe("GovernanceCanister", () => {
         include_neurons_readable_by_caller: true,
         include_empty_neurons_readable_by_caller: [],
         include_public_neurons_in_full_neurons: [],
-        page_number: [],
-        page_size: [],
+        page_number: [1n],
+        page_size: [500n],
         neuron_subaccounts: [],
       });
       expect(service.list_neurons).toBeCalledTimes(1);

packages/nns/src/canisters/governance/request.converters.ts
@@ -1019,19 +1019,23 @@ export const fromListNeurons = ({
   includeEmptyNeurons,
   includePublicNeurons,
   neuronSubaccounts,
+  pageNumber,
+  pageSize,
 }: {
   neuronIds?: NeuronId[];
   includeEmptyNeurons?: boolean;
   includePublicNeurons?: boolean;
   neuronSubaccounts?: NeuronSubaccount[];
+  pageNumber?: bigint;
+  pageSize?: bigint;
 }): RawListNeurons => ({
   neuron_ids: BigUint64Array.from(neuronIds ?? []),
   include_neurons_readable_by_caller: neuronIds ? false : true,
   include_empty_neurons_readable_by_caller: toNullable(includeEmptyNeurons),
   include_public_neurons_in_full_neurons: toNullable(includePublicNeurons),
-  page_number: [],
-  page_size: [],
   neuron_subaccounts: toNullable(neuronSubaccounts),
+  page_number: toNullable(pageNumber),
+  page_size: toNullable(pageSize),
 });
 
 export const fromManageNeuron = ({

packages/nns/src/mocks/governance.mock.ts
@@ -54,5 +54,5 @@ export const mockNeuron: Neuron = {
 export const mockListNeuronsResponse: ListNeuronsResponse = {
   neuron_infos: [[mockNeuronId, mockNeuronInfo]],
   full_neurons: [mockNeuron],
-  total_pages_available: [],
+  total_pages_available: [1n],
 };

commit fcbba69c5fc7c0825d16e4b6c74e22196d658d8a

fix(pagination): correct initial page number for list_neurons pagination (#843)
# Motivation

A bug was introduced in #833 in this
[commit](https://github.com/dfinity/ic-js/pull/833/commits/45eb69de418cc22a7b7f8a9da2eb80db47a8c414#diff-f4f0835b56118edf98f5b690eb524de8bd8738c8d742c69c3c2cd516d55e9857R174)
as the initial page should be `0n`, and not `1n`, as shown
[here](https://github.com/dfinity/ic/blob/a34724ef3feef15510ab055b4f5e4aa961ce9e86/rs/nns/governance/src/governance.rs#L2085).
The bug was found through [this failing e2e
pipeline](https://github.com/dfinity/nns-dapp/pull/6419).

# Changes

- Changed the initially requested page from `1n` to `0n`.

# Tests

- Update expectations for the requested page.
- Manually tested in
[devenv](https://qsgjb-riaaa-aaaaa-aaaga-cai.yhabib-ingress.devenv.dfinity.network/neurons/?u=qsgjb-riaaa-aaaaa-aaaga-cai)
- Tested against [e2e
test](https://github.com/dfinity/nns-dapp/pull/6428) from nns-dapp

# Todos

- [ ] Add entry to changelog (if necessary).

packages/nns/src/governance.canister.ts
@@ -178,7 +178,7 @@ export class GovernanceCanister {
       includeEmptyNeurons,
       includePublicNeurons,
       neuronSubaccounts,
-      pageNumber: 1n,
+      pageNumber: 0n,
     });
     const { neurons: firstPageNeurons, totalPages } = firstPageResult;
 

packages/nns/src/governance.canister.spec.ts
@@ -459,7 +459,7 @@ describe("GovernanceCanister", () => {
         include_empty_neurons_readable_by_caller: [true],
         include_public_neurons_in_full_neurons: [true],
         neuron_subaccounts: [],
-        page_number: [1n],
+        page_number: [0n],
         page_size: [500n],
       });
       expect(neurons.length).toBe(1);
@@ -502,7 +502,7 @@ describe("GovernanceCanister", () => {
         include_empty_neurons_readable_by_caller: [true],
         include_public_neurons_in_full_neurons: [true],
         neuron_subaccounts: [],
-        page_number: [1n],
+        page_number: [0n],
         page_size: [500n],
       });
 
@@ -543,7 +543,7 @@ describe("GovernanceCanister", () => {
         include_empty_neurons_readable_by_caller: [false],
         include_public_neurons_in_full_neurons: [],
         neuron_subaccounts: [],
-        page_number: [1n],
+        page_number: [0n],
         page_size: [500n],
       });
       expect(service.list_neurons).toBeCalledTimes(1);
@@ -570,7 +570,7 @@ describe("GovernanceCanister", () => {
         include_empty_neurons_readable_by_caller: [],
         include_public_neurons_in_full_neurons: [false],
         neuron_subaccounts: [],
-        page_number: [1n],
+        page_number: [0n],
         page_size: [500n],
       });
       expect(service.list_neurons).toBeCalledTimes(1);
@@ -601,7 +601,7 @@ describe("GovernanceCanister", () => {
         include_neurons_readable_by_caller: true,
         include_empty_neurons_readable_by_caller: [true],
         include_public_neurons_in_full_neurons: [true],
-        page_number: [1n],
+        page_number: [0n],
         page_size: [500n],
         neuron_subaccounts: [
           [
@@ -665,7 +665,7 @@ describe("GovernanceCanister", () => {
         include_neurons_readable_by_caller: true,
         include_empty_neurons_readable_by_caller: [],
         include_public_neurons_in_full_neurons: [],
-        page_number: [1n],
+        page_number: [0n],
         page_size: [500n],
         neuron_subaccounts: [],
       });