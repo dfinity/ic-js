552aa0a43cf30085a8c8c12ecc4cfef5d43b0630
Llorenç <llorenc.muntaner@dfinity.org>
2025-03-14 13:15:28 +0100

    New script to create supported tokens

---
+++ packages/ledger-icrc/candid/icrc_ledger.idl.cjs
@@ -0,0 +1,34 @@
+const idlFactory = ({ IDL }) => {
+  const Value = IDL.Variant({
+    Int: IDL.Int,
+    Nat: IDL.Nat,
+    Blob: IDL.Vec(IDL.Nat8),
+    Text: IDL.Text,
+  });
+
+  return IDL.Service({
+    icrc1_balance_of: IDL.Func(
+      [
+        IDL.Record({
+          owner: IDL.Principal,
+          subaccount: IDL.Opt(IDL.Vec(IDL.Nat8)),
+        }),
+      ],
+      [IDL.Nat],
+      ["query"],
+    ),
+    icrc1_metadata: IDL.Func(
+      [],
+      [IDL.Vec(IDL.Tuple(IDL.Text, Value))],
+      ["query"],
+    ),
+    icrc1_name: IDL.Func([], [IDL.Text], ["query"]),
+    icrc1_symbol: IDL.Func([], [IDL.Text], ["query"]),
+    icrc1_decimals: IDL.Func([], [IDL.Nat8], ["query"]),
+    icrc1_fee: IDL.Func([], [IDL.Nat], ["query"]),
+    icrc1_total_supply: IDL.Func([], [IDL.Nat], ["query"]),
+    // Add any other methods you need here
+  });
+};
+
+module.exports = { idlFactory };
