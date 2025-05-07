dc289e672aa1edefd35be6cbeda7944d4d63322a
David Dal Busco <david.dalbusco@dfinity.org>
2025-05-07 08:02:40 +0200

    chore: eslint prefer-template on
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,7 +24,6 @@ export default [
       "no-else-return": "off",
       "@typescript-eslint/no-unsafe-function-type": "off",
       "@typescript-eslint/consistent-type-imports": "off",
-      "prefer-template": "off",
       curly: "off",
     },
   },
