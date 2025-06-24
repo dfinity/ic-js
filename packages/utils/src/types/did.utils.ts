97592a479452b65eb456f62a79c644aea660c401
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-06-24 17:50:57 +0200

    jsdoc

--- packages/utils/src/types/did.utils.ts
+++ packages/utils/src/types/did.utils.ts
@@ -1,3 +1,11 @@
+/**
+ * A Candid-style variant representation of an optional value.
+ * It is an empty array for `null` and `undefined`, or an array with the value.
+ */
 export type Nullable<T> = [] | [T];
 
+/**
+ * An optional Candid-style variant representation of an optional value.
+ * It is an empty array for `null` and `undefined`, or an array with the value, or `undefined`.
+ */
 export type NullishNullable<T> = Nullable<T> | undefined;
