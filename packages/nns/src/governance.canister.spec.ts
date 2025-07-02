44aefbb588709452a8639fe074bac8cb80fd1465
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-07-02 08:47:57 +0200

    tests

--- packages/nns/src/governance.canister.spec.ts
+++ packages/nns/src/governance.canister.spec.ts
@@ -15,8 +15,10 @@ import type {
   ManageNeuronRequest,
   ManageNeuronResponse,
   NeuronsFundEconomics,
+  GovernanceCachedMetrics as RawGovernanceCachedMetrics,
   InstallCode as RawInstallCode,
   NetworkEconomics as RawNetworkEconomics,
+  NeuronSubsetMetrics as RawNeuronSubsetMetrics,
   ProposalInfo as RawProposalInfo,
   StopOrStartCanister as RawStopOrStartCanister,
   UpdateCanisterSettings as RawUpdateCanisterSettings,
@@ -47,6 +49,7 @@ import {
 } from "./mocks/governance.mock";
 import type {
   Action,
+  GovernanceCachedMetrics,
   InstallCode,
   MakeProposalRequest,
   NetworkEconomics,
@@ -2384,6 +2387,211 @@ describe("GovernanceCanister", () => {
         ],
         neuron_id_or_subaccount: [],
       } as ManageNeuronRequest);
+    });
+  });
+
+  describe("getMetrics", () => {
+    const rawNeuronSubsetMetrics: RawNeuronSubsetMetrics = {
+      total_maturity_e8s_equivalent: [BigInt(100)],
+      maturity_e8s_equivalent_buckets: [[BigInt(1), BigInt(2)]],
+      voting_power_buckets: [[BigInt(1), BigInt(2)]],
+      total_staked_e8s: [BigInt(2)],
+      count: [],
+      deciding_voting_power_buckets: [[BigInt(1), BigInt(2)]],
+      total_staked_maturity_e8s_equivalent: [BigInt(7)],
+      total_potential_voting_power: [BigInt(7)],
+      total_deciding_voting_power: [BigInt(7)],
+      staked_maturity_e8s_equivalent_buckets: [[BigInt(1), BigInt(2)]],
+      staked_e8s_buckets: [[BigInt(1), BigInt(2)]],
+      total_voting_power: [BigInt(7)],
+      potential_voting_power_buckets: [[BigInt(1), BigInt(2)]],
+      count_buckets: [[BigInt(1), BigInt(2)]],
+    };
+    const rawMetrics: RawGovernanceCachedMetrics = {
+      total_maturity_e8s_equivalent: BigInt(100_000_000_000),
+      not_dissolving_neurons_e8s_buckets: [[BigInt(10), 1]],
+      dissolving_neurons_staked_maturity_e8s_equivalent_sum: BigInt(50_000),
+      garbage_collectable_neurons_count: BigInt(5),
+      dissolving_neurons_staked_maturity_e8s_equivalent_buckets: [
+        [BigInt(1), 2],
+      ],
+      neurons_with_invalid_stake_count: BigInt(3),
+      not_dissolving_neurons_count_buckets: [[BigInt(2), BigInt(2)]],
+      ect_neuron_count: BigInt(100),
+      total_supply_icp: BigInt(1_000_000),
+      neurons_with_less_than_6_months_dissolve_delay_count: BigInt(10),
+      dissolved_neurons_count: BigInt(20),
+      community_fund_total_maturity_e8s_equivalent: BigInt(4),
+      total_staked_e8s_seed: BigInt(4),
+      total_staked_maturity_e8s_equivalent_ect: BigInt(4),
+      total_staked_e8s: BigInt(4),
+      fully_lost_voting_power_neuron_subset_metrics: [rawNeuronSubsetMetrics],
+      not_dissolving_neurons_count: BigInt(100),
+      total_locked_e8s: BigInt(3),
+      neurons_fund_total_active_neurons: BigInt(3),
+      total_voting_power_non_self_authenticating_controller: [],
+      total_staked_maturity_e8s_equivalent: BigInt(3),
+      not_dissolving_neurons_e8s_buckets_ect: [],
+      declining_voting_power_neuron_subset_metrics: [rawNeuronSubsetMetrics],
+      total_staked_e8s_ect: BigInt(3),
+      not_dissolving_neurons_staked_maturity_e8s_equivalent_sum: BigInt(3),
+      dissolved_neurons_e8s: BigInt(3),
+      total_staked_e8s_non_self_authenticating_controller: [],
+      dissolving_neurons_e8s_buckets_seed: [],
+      neurons_with_less_than_6_months_dissolve_delay_e8s: BigInt(3),
+      not_dissolving_neurons_staked_maturity_e8s_equivalent_buckets: [],
+      dissolving_neurons_count_buckets: [],
+      dissolving_neurons_e8s_buckets_ect: [],
+      non_self_authenticating_controller_neuron_subset_metrics: [
+        rawNeuronSubsetMetrics,
+      ],
+      dissolving_neurons_count: BigInt(8),
+      dissolving_neurons_e8s_buckets: [],
+      total_staked_maturity_e8s_equivalent_seed: BigInt(8),
+      community_fund_total_staked_e8s: BigInt(8),
+      not_dissolving_neurons_e8s_buckets_seed: [],
+      public_neuron_subset_metrics: [rawNeuronSubsetMetrics],
+      timestamp_seconds: BigInt(8),
+      seed_neuron_count: BigInt(8),
+    };
+    const mockMetrics: GovernanceCachedMetrics = {
+      communityFundTotalMaturityE8sEquivalent: 4n,
+      communityFundTotalStakedE8s: 8n,
+      decliningVotingPowerNeuronSubsetMetrics: {
+        count: undefined,
+        countBuckets: undefined,
+        decidingVotingPowerBuckets: undefined,
+        maturityE8sEquivalentBuckets: undefined,
+        potentialVotingPowerBuckets: undefined,
+        stakedE8sBuckets: undefined,
+        stakedMaturityE8sEquivalentBuckets: undefined,
+        totalDecidingVotingPower: undefined,
+        totalMaturityE8sEquivalent: undefined,
+        totalPotentialVotingPower: undefined,
+        totalStakedE8s: undefined,
+        totalStakedMaturityE8sEquivalent: undefined,
+        totalVotingPower: undefined,
+        votingPowerBuckets: undefined,
+      },
+      dissolvedNeuronsCount: 20n,
+      dissolvedNeuronsE8s: 3n,
+      dissolvingNeuronsCount: 8n,
+      dissolvingNeuronsCountBuckets: [],
+      dissolvingNeuronsE8sBuckets: [],
+      dissolvingNeuronsE8sBucketsEct: [],
+      dissolvingNeuronsE8sBucketsSeed: [],
+      dissolvingNeuronsStakedMaturityE8sEquivalentBuckets: [[1n, 2]],
+      dissolvingNeuronsStakedMaturityE8sEquivalentSum: 50000n,
+      ectNeuronCount: 100n,
+      fullyLostVotingPowerNeuronSubsetMetrics: {
+        count: undefined,
+        countBuckets: undefined,
+        decidingVotingPowerBuckets: undefined,
+        maturityE8sEquivalentBuckets: undefined,
+        potentialVotingPowerBuckets: undefined,
+        stakedE8sBuckets: undefined,
+        stakedMaturityE8sEquivalentBuckets: undefined,
+        totalDecidingVotingPower: undefined,
+        totalMaturityE8sEquivalent: undefined,
+        totalPotentialVotingPower: undefined,
+        totalStakedE8s: undefined,
+        totalStakedMaturityE8sEquivalent: undefined,
+        totalVotingPower: undefined,
+        votingPowerBuckets: undefined,
+      },
+      garbageCollectableNeuronsCount: 5n,
+      neuronsFundTotalActiveNeurons: 3n,
+      neuronsWithInvalidStakeCount: 3n,
+      neuronsWithLessThan6MonthsDissolveDelayCount: 10n,
+      neuronsWithLessThan6MonthsDissolveDelayE8s: 3n,
+      nonSelfAuthenticatingControllerNeuronSubsetMetrics: {
+        count: undefined,
+        countBuckets: undefined,
+        decidingVotingPowerBuckets: undefined,
+        maturityE8sEquivalentBuckets: undefined,
+        potentialVotingPowerBuckets: undefined,
+        stakedE8sBuckets: undefined,
+        stakedMaturityE8sEquivalentBuckets: undefined,
+        totalDecidingVotingPower: undefined,
+        totalMaturityE8sEquivalent: undefined,
+        totalPotentialVotingPower: undefined,
+        totalStakedE8s: undefined,
+        totalStakedMaturityE8sEquivalent: undefined,
+        totalVotingPower: undefined,
+        votingPowerBuckets: undefined,
+      },
+      notDissolvingNeuronsCount: 100n,
+      notDissolvingNeuronsCountBuckets: [[2n, 2n]],
+      notDissolvingNeuronsE8sBuckets: [[10n, 1]],
+      notDissolvingNeuronsE8sBucketsEct: [],
+      notDissolvingNeuronsE8sBucketsSeed: [],
+      notDissolvingNeuronsStakedMaturityE8sEquivalentBuckets: [],
+      notDissolvingNeuronsStakedMaturityE8sEquivalentSum: 3n,
+      publicNeuronSubsetMetrics: {
+        count: undefined,
+        countBuckets: undefined,
+        decidingVotingPowerBuckets: undefined,
+        maturityE8sEquivalentBuckets: undefined,
+        potentialVotingPowerBuckets: undefined,
+        stakedE8sBuckets: undefined,
+        stakedMaturityE8sEquivalentBuckets: undefined,
+        totalDecidingVotingPower: undefined,
+        totalMaturityE8sEquivalent: undefined,
+        totalPotentialVotingPower: undefined,
+        totalStakedE8s: undefined,
+        totalStakedMaturityE8sEquivalent: undefined,
+        totalVotingPower: undefined,
+        votingPowerBuckets: undefined,
+      },
+      seedNeuronCount: 8n,
+      timestampSeconds: 8n,
+      totalLockedE8s: 3n,
+      totalMaturityE8sEquivalent: 100000000000n,
+      totalStakedE8s: 4n,
+      totalStakedE8sEct: 3n,
+      totalStakedE8sNonSelfAuthenticatingController: undefined,
+      totalStakedE8sSeed: 4n,
+      totalStakedMaturityE8sEquivalent: 3n,
+      totalStakedMaturityE8sEquivalentEct: 4n,
+      totalStakedMaturityE8sEquivalentSeed: 8n,
+      totalSupplyIcp: 1000000n,
+      totalVotingPowerNonSelfAuthenticatingController: undefined,
+    };
+
+    it("gets ths metrics parameters", async () => {
+      const service = mock<ActorSubclass<GovernanceService>>();
+      service.get_metrics.mockResolvedValue({
+        Ok: rawMetrics,
+      });
+
+      const governance = GovernanceCanister.create({
+        certifiedServiceOverride: service,
+        serviceOverride: service,
+      });
+      const response = await governance.getMetrics({
+        certified: true,
+      });
+
+      expect(service.get_metrics).toHaveBeenCalledTimes(1);
+      expect(response).toEqual(mockMetrics);
+    });
+
+    it("should throw an error", async () => {
+      const service = mock<ActorSubclass<GovernanceService>>();
+      service.get_metrics.mockResolvedValue({
+        Err: { error_message: "Oh No", error_type: 2 },
+      });
+
+      const governance = GovernanceCanister.create({
+        certifiedServiceOverride: service,
+        serviceOverride: service,
+      });
+      const response = governance.getMetrics({
+        certified: true,
+      });
+
+      await expect(response).rejects.toThrowError();
+      expect(service.get_metrics).toHaveBeenCalledTimes(1);
     });
   });
 });
