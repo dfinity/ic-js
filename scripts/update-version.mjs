ae6dc915e2b82b973df27e214f5eaca5bcda4177
David Dal Busco <david.dalbusco@dfinity.org>
2025-07-28 06:11:34 +0200

    chore: pin agent-js
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- scripts/update-version.mjs
+++ scripts/update-version.mjs
@@ -49,12 +49,16 @@ const updateVersion = async () => {
   });
 
   // Peer dependencies need to point to wip references - e.g. @dfinity/utils@0.0.1-next
-  const peerDependencies = Object.entries(
-    packageJson.peerDependencies ?? {},
-  ).reduce((acc, [key, _value]) => {
-    acc[key] = `*`;
-    return acc;
-  }, {});
+  const peerDependencies =
+    tag !== "next"
+      ? {}
+      : Object.entries(packageJson.peerDependencies ?? {}).reduce(
+          (acc, [key, _value]) => {
+            acc[key] = `*`;
+            return acc;
+          },
+          {},
+        );
 
   writeFileSync(
     packagePath,
