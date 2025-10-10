1aba656b11789a7d9b180e8b31783e98e0c1bfe0
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-10 10:24:51 +0200

    more

--- packages/ledger-icrc/src/converters/converters.ts
+++ packages/ledger-icrc/src/converters/converters.ts
@@ -3,7 +3,7 @@ import type { Account } from "../../candid/icrc_ledger";
 import type { IcrcAccount } from "../types/ledger.responses";
 
 /**
- * Converts a Candid Account object to an IcrcAccount.
+ * Converts a Candid Account object to an IcrcAccount, effectively transforming nullable properties into nullish ones.
  *
  * @param {Account} - The Candid Account object to convert.
  * @return {IcrcAccount} - The converted IcrcAccount object.
