d05e1cd557decac89e2934b2a442456290ffc328
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-10-15 14:09:11 +0200

    fix import

--- packages/nns/src/types/governance_converters.ts
+++ packages/nns/src/types/governance_converters.ts
@@ -1,7 +1,7 @@
 import type { DerEncodedPublicKey } from "@dfinity/agent";
 import type { AccountIdentifierHex } from "@dfinity/ledger-icp";
 import type { Principal } from "@dfinity/principal";
-import { TopicToFollow } from "../../candid/governance";
+import type { TopicToFollow } from "../../candid/governance";
 import type {
   CanisterAction,
   CanisterInstallMode,
