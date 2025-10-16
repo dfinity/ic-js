540279369a56c52d845b1fefbd159e9a3f1d6f3d
David Dal Busco <david.dalbusco@dfinity.org>
2025-10-16 11:54:03 +0200

    feat(canisters): icp-sdk/canisters should reexport cketh (#1126)
    # Motivation
    
    The `@icp-sdk/canisters` package is meant to re-export the existing
    DFINITY libraries related to canisters. This PR provides the re-export
    for `@dfinity/cketh`.
    
    # Notes
    
    The peer dependency is added with a pinned requirement. Given that we
    are going to release both the DFINITY and `@icp-sdk/canisters` libraries
    in parallel, we want to enforce that developers use the latest version
    to inherit what is actually shipped and specified in the CHANGELOG.
    
    # Changes
    
    - Add peer dependencies  
    - `export * from ...`

---
+++ packages/canisters/src/cketh.spec.ts
@@ -0,0 +1,17 @@
+import { CkETHMinterCanister, CkETHOrchestratorCanister } from "./cketh";
+import { shouldReExportAllMembers } from "./utils/test.utils";
+
+describe("@icp-sdk/cketh", () => {
+  it("should re-export CkETHMinterCanister", () => {
+    expect(CkETHMinterCanister).not.toBeUndefined();
+  });
+
+  it("should re-export CkETHOrchestratorCanister", () => {
+    expect(CkETHOrchestratorCanister).not.toBeUndefined();
+  });
+
+  shouldReExportAllMembers({
+    source: "@dfinity/cketh",
+    reexport: "@icp-sdk/canisters/cketh",
+  });
+});
