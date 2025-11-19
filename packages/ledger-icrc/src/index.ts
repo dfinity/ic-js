1055e114c380aac42bdde178e5085dfcb5f29f1e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-19 11:11:45 +0100

    index

--- packages/ledger-icrc/src/index.ts
+++ packages/ledger-icrc/src/index.ts
@@ -26,6 +26,7 @@ export * from "./errors/ledger.errors";
 export { IcrcIndexNgCanister } from "./index-ng.canister";
 export { IcrcIndexCanister } from "./index.canister";
 export { IcrcLedgerCanister } from "./ledger.canister";
+export { IcrcNftLedgerCanister } from "./nft-ledger.canister";
 export type * from "./types/index-ng.params";
 export type * from "./types/index-ng.types";
 export type * from "./types/index.params";
