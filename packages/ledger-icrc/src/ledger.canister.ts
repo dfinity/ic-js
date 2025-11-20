05400e66b46ac4c097b57fd14ba69ade573d9ea6
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-20 15:54:07 +0100

    revert

--- packages/ledger-icrc/src/ledger.canister.ts
+++ packages/ledger-icrc/src/ledger.canister.ts
@@ -222,6 +222,18 @@ export class IcrcLedgerCanister extends IcrcCanister<IcrcLedgerService> {
     this.caller(params).icrc1_supported_standards();
 
   /**
+   * Returns the list of standards this ledger supports by using icrc10_supported_standards.
+   *
+   * @link: https://github.com/dfinity/ICRC/blob/main/ICRCs/ICRC-10/ICRC-10.md#icrc10_supported_standards
+   *
+   * @returns {Promise<{ url: string; name: string }[]>} The list of standards.
+   */
+  icrc10SupportedStandards = (
+    params: QueryParams,
+  ): Promise<{ url: string; name: string }[]> =>
+    this.caller(params).icrc10_supported_standards();
+
+  /**
    * Returns the minting account of the Ledger canister.
    *
    * @link: https://github.com/dfinity/ICRC-1/blob/main/standards/ICRC-1/README.md#icrc1_minting_account
